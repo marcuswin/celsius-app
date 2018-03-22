402b1623911d520d385717ef12692180e93da167
Djordje Stevanovic <dsdjolence@gmail.com>
2018-03-22 16:46:35 +0100

    #CN-13: Apply for the loan (name, address, bank account number, etc...) - refactor actions

--- app/redux/reducers/nav.js
+++ app/redux/reducers/nav.js
@@ -2,7 +2,7 @@ import AppNavigator from '../../config/Navigator';
 
 const initialState = {
   index: 0,
-  routes: [{ key: 'Init', routeName: 'BankAccountInfo' }],
+  routes: [{ key: 'Init', routeName: 'Welcome' }],
 };
 
 export default (state = initialState, action) =>
