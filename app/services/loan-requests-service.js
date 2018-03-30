28a1e7142617abd6ca51025f9db2d73e047f81c1
fj <filip.jovakaric@gmail.com>
2018-03-30 13:44:09 +0200

    CN-138 upgrade loanRequest service/actions/reducer

--- app/services/loan-requests-service.js
+++ app/services/loan-requests-service.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 import apiUrl from './api-url';
 
 const loanRequestsService = {
+  get,
   create,
   accept,
   getSupportedCurrencies,
+  createLoanInfo,
 };
+
+function get() {
+  return axios.get(`${apiUrl}/loan_requests`);
+}
 
 function create(loanCollaterals) {
   return axios.post(`${apiUrl}/loan_requests`, {
@@ -19,6 +25,13 @@ function accept(id) {
 
 function getSupportedCurrencies() {
   return axios.get(`${apiUrl}/loan_requests/supported_currencies`);
+}
+
+function createLoanInfo(loanRequestId, loanInfo) {
+  return axios.post(`${apiUrl}/loan_requests/${loanRequestId}/loan_info`, {
+    loan_purpose: loanInfo.purposeOfLoan,
+    note: loanInfo.note,
+  })
 }
 
 export default loanRequestsService;
