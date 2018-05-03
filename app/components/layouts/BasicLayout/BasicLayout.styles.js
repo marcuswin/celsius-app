ff81a27af23f8e3f63e7f8a4f450313e1987cf5e
fj <filip.jovakaric@gmail.com>
2018-05-04 01:07:11 +0200

    CN-298: creating BottomNavigation and basic layouts

---
+++ app/components/layouts/BasicLayout/BasicLayout.styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+// import {FONT_SCALE, STYLES} from "../../../config/constants/style";
+
+
+const BasicLayoutStyle = StyleSheet.create({
+
+});
+
+export default BasicLayoutStyle;
