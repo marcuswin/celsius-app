bbd3ba15b337ba040a9a78f2a992a8e6b90b2695
fj <filip.jovakaric@gmail.com>
2018-05-07 23:36:21 +0200

    CN-312: adding & updating component

---
+++ app/components/organisms/SelectCountry/SelectCountry.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, View } from 'react-native';
+
+import PrimaryInput from '../../atoms/Inputs/PrimaryInput';
+import SelectCountryModal from '../../organisms/SelectCountryModal/SelectCountryModal';
+
+class SelectCountry extends Component {
+  static propTypes = {
+    setCountry: PropTypes.func.isRequired,
+    country: PropTypes.string,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalVisible: false,
+    };
+
+    // binders
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  // lifecycle methods
+  // event hanlders
+  closeModal(country) {
+    const { setCountry } = this.props;
+    setCountry(country);
+    this.setState({ modalVisible: false });
+  }
+  // rendering methods
+  render() {
+    const { modalVisible } = this.state;
+    const { country } = this.props;
+
+    return (
+      <View>
+        <TouchableOpacity onPress={() => this.setState({modalVisible: true})}>
+          <PrimaryInput
+            clickable
+            labelText="Country" value={country}
+            onPress={() => this.setState({modalVisible: true})}/>
+        </TouchableOpacity>
+
+        <SelectCountryModal
+          visible={modalVisible}
+          onClose={this.closeModal}
+        />
+      </View>
+    );
+  }
+}
+
+export default SelectCountry;
