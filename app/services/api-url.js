725d41d7920b548703d799c5cfd01f661344ed86
Djordje Stevanovic <dsdjolence@gmail.com>
2018-05-08 13:48:47 +0200

    CN-216: Frontend environment setup

--- app/services/api-url.js
+++ app/services/api-url.js
@@ -1,13 +1,5 @@
-import {API_V1_URL_PRODUCTION, API_V1_URL_DEVELOP, API_V1_URL_STAGING} from 'react-native-dotenv'
 import {Constants} from 'expo';
 
-function getApiUrl(releaseChannel) {
-  if (releaseChannel === undefined) return API_V1_URL_DEVELOP; // since releaseChannels are undefined in dev, return your default.
-  if (releaseChannel.indexOf('staging') !== -1) return API_V1_URL_STAGING;
-  if (releaseChannel.indexOf('production') !== -1) return API_V1_URL_PRODUCTION;
-}
+const {API_URL} = Constants.manifest.extra;
 
-const releaseChanel = Constants.manifest.releaseChannel;
-const baseUrl = getApiUrl(releaseChanel);
-
-export default baseUrl;
+export default API_URL;
