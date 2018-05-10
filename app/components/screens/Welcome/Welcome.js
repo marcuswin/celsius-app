5dd4ea19d7bdc9990ce35d206a6f713070a5ccef
Nikola Sabelnik <sabelnikola@gmail.com>
2018-05-10 13:24:55 +0200

    CN-273: creating welcome screen slider

--- app/components/screens/Welcome/Welcome.js
+++ app/components/screens/Welcome/Welcome.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
-import {Text, StatusBar, Image} from 'react-native';
+import {StatusBar} from 'react-native';
 import {connect} from 'react-redux';
 import {Container, Content, View} from 'native-base';
 import {bindActionCreators} from 'redux';
 
 import {MainHeader} from '../../molecules/MainHeader/MainHeader';
-import {GLOBAL_STYLE_DEFINITIONS} from "../../../config/constants/style";
 import WelcomeStyle from "./Welcome.styles";
 import CelButton from "../../atoms/CelButton/CelButton";
 import * as actions from '../../../redux/actions';
+import WelcomeCarousel from "../../molecules/WelcomeCarousel/WelcomeCarousel";
 
 @connect(
   state => ({
@@ -39,16 +39,8 @@ class WelcomeScreen extends Component {
         <Content bounces={false} style={WelcomeStyle.content}>
           <View style={[WelcomeStyle.view]}>
 
-            <View style={[WelcomeStyle.heroImageWrapper, GLOBAL_STYLE_DEFINITIONS.centeredColumn]}>
-              <Image source={require('../../../../assets/images/Welcome-Animal.png')} style={[WelcomeStyle.heroImage]}/>
-            </View>
+            <WelcomeCarousel />
 
-            <Text style={WelcomeStyle.heading}>Welcome to Celsius</Text>
-            <Text style={WelcomeStyle.description}>
-              We are creating the future of
-              <Text style={GLOBAL_STYLE_DEFINITIONS.boldText}> P2P lending and borrowing</Text>.
-              Keep up with what we’re doing by joining our community of members.
-            </Text>
             <View style={WelcomeStyle.buttonWrapper}>
               <CelButton
                 onPress={() => navigateTo('SignupOne')}
