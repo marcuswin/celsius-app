579d24825f77c325f36a56ceb61f5a86dda412d7
Nikola Sabelnik <sabelnikola@gmail.com>
2018-05-10 13:24:55 +0200

    CN-273: creating welcome screen slider

---
+++ app/components/molecules/WelcomeCarousel/WelcomeCarousel.styles.js
@@ -0,0 +1,79 @@
+import { Dimensions, StyleSheet } from "react-native";
+import { FONT_SCALE } from "../../../config/constants/style";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
+const WelcomeCarouselStyle = StyleSheet.create({
+
+    scrollPage: {
+      width: SCREEN_WIDTH - 80,
+    },
+    screen: {
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    title: {
+      fontFamily: 'agile-bold',
+      fontWeight: "800",
+      fontSize: FONT_SCALE * 42,
+      color: '#FFFFFF',
+      textAlign: 'center',
+      lineHeight: 40,
+    },
+    title2: {
+      fontFamily: 'agile-bold',
+      fontWeight: "800",
+      fontSize: FONT_SCALE * 42,
+      color: '#FFFFFF',
+      textAlign: 'center',
+      lineHeight: 40,
+      paddingBottom: 10,
+    },
+    image: {
+      width: 250,
+      height: 150,
+    },
+    smallDescription: {
+      fontFamily: 'agile-bold',
+      fontWeight: "500",
+      fontSize: FONT_SCALE * 16,
+      color: '#88A2C7',
+      paddingBottom: 19,
+      lineHeight: 25,
+    },
+    largeDescription: {
+      fontFamily: 'agile-light',
+      fontWeight: "300",
+      fontSize: FONT_SCALE * 18,
+      color: '#FFFFFF',
+      textAlign: 'center',
+    },
+    circle: {
+      margin: 12,
+      width: 8,
+      height: 8,
+      borderRadius: 8/2,
+      backgroundColor: 'rgba(255,255,255,0.2)',
+    },
+    circleActive: {
+      margin: 12,
+      width: 8,
+      height: 8,
+      borderRadius: 8/2,
+      backgroundColor: 'rgba(255,255,255,0.6)',
+    },
+    circleWraper: {
+      flexDirection: 'row',
+      justifyContent: "center",
+      alignItems: "center",
+      paddingBottom: 10,
+    },
+    circleHighlight: {
+      width: 8,
+      height: 8,
+      borderRadius: 8/2,
+      backgroundColor: '#FFFFFF',
+    },
+});
+
+export default WelcomeCarouselStyle;
