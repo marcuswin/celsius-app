7bccc9e902e2c14ae9553b3fd2d47c3268337c1e
fj <filip.jovakaric@gmail.com>
2018-05-16 12:44:00 +0200

    CN-380 Login Screen updates

--- app/components/screens/Login/Login.js
+++ app/components/screens/Login/Login.js
@@ -1,35 +1,19 @@
 import React, {Component} from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {Text} from 'react-native';
 import {connect} from 'react-redux';
-import {Container, Content, View} from 'native-base';
+import {View} from 'native-base';
 import {bindActionCreators} from 'redux';
-import {Col, Grid} from "react-native-easy-grid";
-import {TWLoginButton} from 'react-native-simple-twitter';
-import {Constants, Facebook, Google} from "expo";
 import {SECURITY_STORAGE_AUTH_KEY} from 'react-native-dotenv'
 
-import {MainHeader} from '../../molecules/MainHeader/MainHeader';
-import {AnimatedHeading} from '../../molecules/AnimatedHeading/AnimatedHeading';
-import {Message} from '../../atoms/Message/Message';
 import CelButton from '../../atoms/CelButton/CelButton';
 import LoginForm from "../../organisms/LoginForm/LoginForm";
 import LoginStyle from "./Login.styles";
 import * as actions from "../../../redux/actions";
 import {getSecureStoreKey} from '../../../utils/expo-storage';
-import StepStyles from "../Signup/Signup.styles";
-import Icon from "../../atoms/Icon/Icon";
 import Separator from "../../atoms/Separator/Separator";
-
-const {
-  GOOGLE_WEB_CLIENT_ID,
-  GOOGLE_ANDROID_STANDALONE_APP_CLIENT_ID,
-  GOOGLE_IOS_STANDALONE_APP_CLIENT_ID,
-  GOOGLE_ANDROID_CLIENT_ID,
-  GOOGLE_IOS_CLIENT_ID,
-  GOOGLE_URL,
-  FACEBOOK_APP_ID,
-  FACEBOOK_URL
-} = Constants.manifest.extra;
+import ThirdPartyLoginSection from "../../organisms/ThirdPartyLoginSection/ThirdPartyLoginSection";
+import SimpleLayout from "../../layouts/SimpleLayout/SimpleLayout";
+import {STYLES} from "../../../config/constants/style";
 
 @connect(
   state => ({
@@ -67,25 +51,6 @@ class LoginScreen extends Component {
     this.heading.animateHeading(event);
   };
 
-  onOpenTwitter = () => {
-    this.fakeTwitterButton.onButtonPress();
-    this.props.twitterOpen();
-  };
-
-  onTwitterSuccess = (twitterUser) => {
-    const {loginTwitter, user} = this.props;
-
-    const u = twitterUser;
-    u.accessToken = user.twitter_oauth_token;
-    u.secret_token = user.twitter_oauth_secret;
-
-    loginTwitter(u);
-  };
-
-  setFakeTwitterButton = (component) => {
-    this.fakeTwitterButton = component;
-  };
-
   getFormHeight = () => {
     const {textHeight, buttonHeight, headingHeight, smIcon, separator} = this.state;
     const {dimensions} = this.props;
@@ -99,154 +64,47 @@ class LoginScreen extends Component {
     })
   };
 
-  googleAuth = async () => {
-    const {loginGoogle} = this.props;
-
-    try {
-      const result = await Google.logInAsync({
-        behavior: 'web',
-        webClientId: GOOGLE_WEB_CLIENT_ID,
-        androidStandaloneAppClientId: GOOGLE_ANDROID_STANDALONE_APP_CLIENT_ID,
-        iosStandaloneAppClientId: GOOGLE_IOS_STANDALONE_APP_CLIENT_ID,
-        androidClientId: GOOGLE_ANDROID_CLIENT_ID,
-        iosClientId: GOOGLE_IOS_CLIENT_ID,
-        scopes: ['profile', 'email'],
-      });
-
-      if (result.type === 'success') {
-        const userInfoResponse = await fetch(GOOGLE_URL, {
-          headers: {Authorization: `Bearer ${result.accessToken}`},
-        });
-
-        const user = await userInfoResponse.json();
-        user.accessToken = result.accessToken;
-
-        loginGoogle(user)
-      } else {
-        return {cancelled: true};
-      }
-    } catch (e) {
-      return {error: true};
-    }
-  };
-
-  facebookAuth = async () => {
-    const {loginFacebook} = this.props;
-
-    try {
-      const {type, token} = await Facebook.logInWithReadPermissionsAsync(FACEBOOK_APP_ID, {
-        permissions: ['public_profile', 'email'],
-      });
-
-      if (type === 'success') {
-        const response = await fetch(`${FACEBOOK_URL}${token}`);
-
-        const user = await response.json();
-        user.accessToken = token;
-
-        loginFacebook(user);
-      }
-    } catch (e) {
-      this.props.showMessage('error', e.message)
-    }
-
-  };
-
   handleLogin = async data => {
     const {loginBorrower} = this.props;
     await loginBorrower(data);
   };
 
   render() {
-    const {navigateTo, twitterClose, twitterGetAccessToken} = this.props;
+    const {navigateTo} = this.props;
 
     const formHeight = this.getFormHeight();
 
     return (
-      <Container>
-        <MainHeader rightLink={{ screen: 'SignupOne', text: 'Sign up' }}/>
+      <SimpleLayout
+        mainHeader={{ back: false, rightLink: { screen: 'SignupOne', text: 'Sign Up' }}}
+        animatedHeading={{ text: 'Welcome Back!' }}
+        bottomNavigation={ false }
+        background={STYLES.PRIMARY_BLUE}
+      >
 
-        <View onLayout={(e) => this.setHeight('heading', e.nativeEvent.layout)}>
-          <AnimatedHeading
-            ref={(heading) => {
-              this.heading = heading;
-            }}
-            text={'Welcome back!'}/>
+        <Text style={LoginStyle.description}>
+          Looks like your session timed out! Please log in again.
+        </Text>
+
+        <View style={{paddingTop: 30}}>
+          <ThirdPartyLoginSection />
         </View>
 
-        <Message/>
+        <Separator>OR LOGIN WITH E-MAIL</Separator>
 
-        <Content
-          bounces={false}
-          style={LoginStyle.content}
-          onScroll={this.onScroll}>
+        <View style={[LoginStyle.formWrapper, {height: formHeight}]}>
+          <LoginForm onSubmit={(data) => this.handleLogin(data)} buttonText={'Log in'}/>
+        </View>
 
-          <View onLayout={(e) => this.setHeight('text', e.nativeEvent.layout)}>
-            <Text style={LoginStyle.description}>
-              Looks like your session timed out! Please log in again.
-            </Text>
-          </View>
-
-          <View style={{paddingTop: 30}}>
-            <Grid>
-              <Col style={StepStyles.centeredColumn}>
-                <TouchableOpacity onPress={this.facebookAuth}>
-                  <Icon name='Facebook' width='75' height='75' viewBox="0 0 80 80" fill='#FFFFFF'/>
-                  <View style={StepStyles.socialNetworkTextWrapper}>
-                    <Text style={StepStyles.socialNetworkDescription}>Login with</Text>
-                    <Text style={StepStyles.socialNetworkName}>Facebook</Text>
-                  </View>
-                </TouchableOpacity>
-              </Col>
-              <Col style={StepStyles.centeredColumn}>
-                <TouchableOpacity onPress={this.googleAuth}>
-                  <Icon name='Google' width='75' height='75' viewBox="0 0 80 80" fill='#FFFFFF'/>
-                  <View style={StepStyles.socialNetworkTextWrapper}>
-                    <Text style={StepStyles.socialNetworkDescription}>Login with</Text>
-                    <Text style={StepStyles.socialNetworkName}>Google</Text>
-                  </View>
-
-
-                </TouchableOpacity>
-              </Col>
-              <Col style={StepStyles.centeredColumn}>
-                <TouchableOpacity onPress={this.onOpenTwitter}>
-                  <Icon name='Twitter' width='75' height='75' viewBox="0 0 80 80" fill='#FFFFFF'/>
-                  <View style={StepStyles.socialNetworkTextWrapper}>
-                    <Text style={StepStyles.socialNetworkDescription}>Login with</Text>
-                    <Text style={StepStyles.socialNetworkName}>Twitter</Text>
-                  </View>
-                </TouchableOpacity>
-              </Col>
-            </Grid>
-          </View>
-
-          <Separator>OR LOGIN WITH E-MAIL</Separator>
-
-          <View style={[LoginStyle.formWrapper, {height: formHeight}]}>
-            <LoginForm onSubmit={(data) => this.handleLogin(data)} buttonText={'Log in'}/>
-          </View>
-
-          <CelButton
-            size="small"
-            transparent
-            margin="25 0 60 0"
-            onPress={() => navigateTo('ForgottenPassword')}
-          >
-            Forgot password?
-          </CelButton>
-
-          <TWLoginButton
-            ref={this.setFakeTwitterButton}
-            style={StepStyles.fakeTwitterButton}
-            onGetAccessToken={twitterGetAccessToken}
-            onSuccess={this.onTwitterSuccess}
-            closeText="< Back to Celsius"
-            onClose={twitterClose}
-            onError={this.handleError}
-          />
-        </Content>
-      </Container>
+        <CelButton
+          size="small"
+          transparent
+          margin="25 0 60 0"
+          onPress={() => navigateTo('ForgottenPassword')}
+        >
+          Forgot password?
+        </CelButton>
+      </SimpleLayout>
     );
   }
 }
