0062fe64898810141d3331087949c79e629e4c43
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-05-18 17:25:39 +0200

    CN-383: adding bottom nav dimensions to redux

--- app/components/layouts/BasicLayout/BasicLayout.js
+++ app/components/layouts/BasicLayout/BasicLayout.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Container } from 'native-base';
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import * as actions from "../../../redux/actions";
+
 import BottomNavigation from "../../organisms/BottomNavigation/BottomNavigation";
 
 
-const BasicLayout = (props) => {
-  const { bottomNavigation } = props;
-  const marginBottom = bottomNavigation ? 90 : 0;
-  return (
-    <Container style={{ marginBottom }}>
-      { props.children }
-      { bottomNavigation ? <BottomNavigation /> : null }
-    </Container>
-  )
+@connect(
+  state => ({
+    bottomNavigationDimensions: state.ui.dimensions.bottomNavigation,
+  }),
+  dispatch => bindActionCreators(actions, dispatch),
+)
+class BasicLayout extends Component {
+  render() {
+    const { bottomNavigation, bottomNavigationDimensions } = this.props;
+    let marginBottom;
+
+    if (bottomNavigation) {
+      marginBottom = bottomNavigationDimensions.height;
+    } else {
+      marginBottom = 0;
+    }
+
+    return (
+      <Container style={{ marginBottom }}>
+        { this.props.children }
+        { bottomNavigation ? <BottomNavigation /> : null }
+      </Container>
+    )
+  }
 }
 
 export default BasicLayout;
--- app/components/organisms/BottomNavigation/BottomNavigation.styles.js
+++ app/components/organisms/BottomNavigation/BottomNavigation.styles.js
@@ -1,19 +1,8 @@
 import { StyleSheet, Dimensions } from 'react-native';
-import device from '../../../utils/device-util';
 
 import {FONT_SCALE, STYLES} from "../../../config/constants/style";
 
-const { height, width } = Dimensions.get('window');
-
-let bottomNavigationHeight;
-let paddingBottom;
-if (device.isiPhoneX()) {
-  bottomNavigationHeight = 87;
-  paddingBottom = 30;
-} else {
-  bottomNavigationHeight = 60;
-  paddingBottom = 5;
-}
+const { width } = Dimensions.get('window');
 
 const baseItem = {
   justifyContent: 'center',
@@ -32,8 +21,6 @@ const baseText = {
 const BottomNavigationStyle = StyleSheet.create({
   container: {
     position: 'absolute',
-    height: bottomNavigationHeight,
-    top: height - bottomNavigationHeight,
     left: 0,
     width,
     backgroundColor: 'rgba(255,255,255,0.82)',
@@ -43,8 +30,6 @@ const BottomNavigationStyle = StyleSheet.create({
     shadowOffset: {width: 0, height: -1},
     shadowOpacity: 0.1,
     shadowRadius: 2,
-
-    paddingBottom,
   },
   itemActive: {
     ...baseItem,
