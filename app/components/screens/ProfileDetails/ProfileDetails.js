e8152a1eddde3e54c63d964a5d424906469b8405
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-05-24 12:04:29 +0200

    CN-439: updating verify profile screen

--- app/components/screens/ProfileDetails/ProfileDetails.js
+++ app/components/screens/ProfileDetails/ProfileDetails.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
-// import {} from 'react-native';
+import { Text } from 'react-native';
 import { Form } from 'native-base';
 import {connect} from 'react-redux';
 import {bindActionCreators} from "redux";
 
 import * as actions from "../../../redux/actions";
-import {STYLES} from "../../../config/constants/style";
+import {STYLES, GLOBAL_STYLE_DEFINITIONS as globalStyles } from "../../../config/constants/style";
 // import ProfileDetailsStyle from "./ProfileDetails.styles";
 import SimpleLayout from "../../layouts/SimpleLayout/SimpleLayout";
 import CelInput from "../../atoms/CelInput/CelInput";
 import CelButton from "../../atoms/CelButton/CelButton";
 import CelSelect from "../../molecules/CelSelect/CelSelect";
+import Separator from "../../atoms/Separator/Separator";
 
 @connect(
   state => ({
@@ -40,21 +41,30 @@ class ProfileDetails extends Component {
         background={STYLES.PRIMARY_BLUE}
         bottomNavigation={false}
       >
+        <Text style={[globalStyles.normalText, { color: 'white' }]}>
+          Please provide us with the information below to get started.
+        </Text>
+
         <Form>
-          <CelSelect field="gender" type="gender" labelText="gender" value={formData.gender} />
           <CelInput value={formData.firstName} field="firstName" labelText="First Name"/>
           <CelInput value={formData.lastName} field="lastName" labelText="Last Name"/>
+          <CelSelect field="citizenship" type="country" labelText="Citizenship" value={formData.citizenship ? formData.citizenship.name : undefined} />
+          <CelSelect field="gender" type="gender" labelText="Gender" value={formData.gender ? formData.gender.label : undefined} />
+
+          <Separator margin="40 0 15 0">ADDRESS INFO</Separator>
+
+          <CelSelect field="country" type="country" labelText="Country" value={formData.country ? formData.country.name : undefined} />
           <CelInput value={formData.city} field="city" labelText="City"/>
           <CelInput value={formData.street} field="street" labelText="Street"/>
           <CelInput value={formData.zip} field="zip" labelText="ZIP Code"/>
-          <CelInput type="password" value={formData.password} field="password" labelText="Yo Yo Yo"/>
         </Form>
 
         <CelButton
           white
-          onPress={() => submitForm('PROFILE_DETAILS', formData)}
+          onPress={() => submitForm('PROFILE_DETAILS')}
+          iconRight="IconArrowRight"
         >
-          Verify yout profile
+          Verify your profile
         </CelButton>
       </SimpleLayout>
     );
