c009f34e11c73a0315143cf25b494cbeacf9d89d
aradnic@gmail.com <aradnic@gmail.com>
2018-05-28 08:28:09 +0200

    Add missing app config folder

---
+++ app/config/Navigator.js
@@ -0,0 +1,97 @@
+import {createStackNavigator} from 'react-navigation';
+
+// screens
+import WelcomeScreen from '../components/screens/Welcome/Welcome';
+import LoginScreen from "../components/screens/Login/Login";
+import ForgottenPasswordScreen from "../components/screens/ForgottenPassword/ForgottenPassword";
+import HomeScreen from "../components/screens/Home/Home";
+import CalculatorScreen from "../components/screens/Calculator/Calculator";
+import ManagePortfolioScreen from "../components/screens/ManagePortfolio";
+import SignupOneScreen from "../components/screens/Signup/SignupOne";
+import SignupTwoScreen from "../components/screens/Signup/SignupTwo";
+import EstimatedLoanScreen from "../components/screens/EstimatedLoan/EstimatedLoan";
+import TermsOfUseScreen from "../components/screens/TermsOfUse/TermsOfUse";
+import ProfileScreen from '../components/screens/Profile/Profile';
+import ChangePasswordScreen from "../components/screens/ChangePassword/ChangePassword";
+import DepositCoinsScreen from "../components/screens/DepositCoins/DepositCoins";
+import ProfileImageScreen from "../components/screens/ProfileImage/ProfileImage";
+import ProfileDetailsScreen from "../components/screens/ProfileDetails/ProfileDetails";
+import VerifyProfileScreen from "../components/screens/VerifyProfile/VerifyProfile";
+import CameraScreen from "../components/screens/Camera/Camera";
+// NOTE(fj): plop screenGen importing new Screen here
+
+const Navigator = createStackNavigator({
+  Welcome: {
+    screen: WelcomeScreen,
+    title: 'Welcome',
+  },
+  Login: {
+    screen: LoginScreen,
+    title: 'Login',
+  },
+  ForgottenPassword: {
+    screen: ForgottenPasswordScreen,
+    title: 'ForgottenPassword',
+  },
+  Home: {
+    screen: HomeScreen,
+    title: 'Home',
+  },
+  ManagePortfolio: {
+    screen: ManagePortfolioScreen,
+    title: 'ManagePortfolio',
+  },
+  Calculator: {
+    screen: CalculatorScreen,
+    title: 'Calculator',
+  },
+  SignupOne: {
+    screen: SignupOneScreen,
+    title: 'SignupOne',
+  },
+  SignupTwo: {
+    screen: SignupTwoScreen,
+    title: 'SignupTwo',
+  },
+  EstimatedLoan: {
+    screen: EstimatedLoanScreen,
+    title: 'EstimatedLoan',
+  },
+  TermsOfUse: {
+    screen: TermsOfUseScreen,
+    title: 'TermsOfUse',
+  },
+  Profile: {
+    screen: ProfileScreen,
+    title: 'Profile',
+  },
+  ChangePassword: {
+    screen: ChangePasswordScreen,
+    title: 'ChangePassword',
+  },
+  DepositCoins: {
+    screen: DepositCoinsScreen,
+    title: 'DepositCoins',
+  },
+  ProfileImage: {
+    screen: ProfileImageScreen,
+    title: 'ProfileImage',
+  },
+  ProfileDetails: {
+    screen: ProfileDetailsScreen,
+    title: 'ProfileDetails',
+  },
+  VerifyProfile: {
+    screen: VerifyProfileScreen,
+    title: 'VerifyProfile',
+  },
+  Camera: {
+    screen: CameraScreen,
+    title: 'Camera',
+  },
+  // NOTE(fj): plop screenGen inserting new Screen here
+}, {
+  headerMode: 'none'
+});
+
+export default Navigator;
---
+++ app/config/constants/common.js
@@ -0,0 +1,53 @@
+export const KEYBOARD_TYPE = {
+  DEFAULT: 'default',
+  NUMERIC: 'numeric',
+  EMAIL: 'email-address',
+  PHONE: 'phone-pad'
+};
+
+export const AUTO_CAPITALIZE = {
+  CHARACTERS: 'characters',   // all characters
+  WORDS: 'words',             // first letter of each word
+  SENTENCES: 'sentences',     // first letter of each sentence
+  NONE: 'none'                // don't auto capitalize anything
+};
+
+export const GENDER = [
+  {label: 'Male', value: 'male'},
+  {label: 'Female', value: 'female'},
+  {label: 'Other', value: 'other'}
+];
+
+export const PERSON_TITLE = [
+  {label: 'Mr.', value: 'mr'},
+  {label: 'Ms.', value: 'ms'},
+  {label: 'Mrs.', value: 'mrs'}
+];
+
+export const PURPOSE_OF_LOAN = [
+  {label: 'Personal Expenses', value: 'Personal Expenses'},
+  {label: 'Pay off debt', value: 'Pay off debt'},
+  {label: 'Stock/Bond Investment', value: 'Stock/Bond Investment'},
+  {label: 'Additional Crypto Investment', value: 'Additional Crypto Investment'},
+  {label: 'Real Estate', value: 'Real Estate'},
+  {label: 'Business Venture (starting a business)', value: 'Business Venture (starting a business)'},
+  {label: 'Other', value: 'Other'},
+];
+
+export const DOCUMENT_TYPE = [
+  {label: 'Passport', value: 'passport', bothSides: false},
+  {label: 'Driving Licence', value: 'driving_licence', bothSides: true},
+  {label: 'National Identity Card', value: 'identity_card', bothSides: true}
+];
+
+export const CAMERA_PHOTOS = {
+  DOCUMENT_FRONT: 'DOCUMENT_FRONT',
+  DOCUMENT_BACK: 'DOCUMENT_BACK',
+  SELFIE: 'SELFIE',
+  PROFILE_PICTURE: 'PROFILE_PICTURE',
+};
+
+export const CAMERA_COPY = {
+  DOCUMENT: 'Please center your document in the marked area. Ensure that there’s enough light in the room for better picture quality.',
+  SELFIE: 'Please center your face in the circle and take a selfie. We need your recent picture to compare it with the one on the document.',
+}
