b6cd54cf64b239a797e85f0b47983e802e3336c3
aradnic@gmail.com <aradnic@gmail.com>
2018-06-12 16:03:30 +0200

    CN-471: Wallet details heading based on type

--- app/components/molecules/WalletDetailsHeading/WalletDetailsHeading.js
+++ app/components/molecules/WalletDetailsHeading/WalletDetailsHeading.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { View, Text } from "native-base";
 import { TouchableOpacity } from 'react-native';
-import {connect} from 'react-redux';
-import {bindActionCreators} from "redux";
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import Proptypes from 'prop-types';
 
 import CelButton from "../../atoms/CelButton/CelButton";
 import formatter from "../../../utils/formatter"
@@ -21,11 +22,40 @@ import Icon from "../../atoms/Icon/Icon";
 
 class WalletDetailsHeading extends Component {
 
+  
+
+  static Proptypes = {
+    type: Proptypes.oneOfType(['total', 'single-coin']),
+    onPressPrevious: Proptypes.func,
+    onPressNext: Proptypes.func,
+  }
+
+  static defaultProps = {
+    type: 'single-coin'
+  }
+
   render() {
+
+    const coin = {
+      totalUSD: 6332900,
+      amount: 500,
+      short: 'BTC',
+    };
+
+    const { type } = this.props;
+
     return <View style={WalletDetailsHeadingStyle.root}>
       <View style={{position: "relative", width: '100%'}}>
-        <Text style={WalletDetailsHeadingStyle.totalValueAmount}>{formatter.usd(6332900)}</Text>
-        <Text style={WalletDetailsHeadingStyle.totalCoinAmount}>100 ETH</Text>
+        <Text style={WalletDetailsHeadingStyle.totalValueAmount}>{formatter.usd(coin.totalUSD)}</Text>
+        <View style={WalletDetailsHeadingStyle.totalCoinAmountWrapper}>
+          {type === 'single-coin' && <Icon name={`Icon${coin.short}`} height='25' width='25' fill="white" viewBox="0 0 49.23 49.23" style={{opacity: .6}} />}
+          <Text style={WalletDetailsHeadingStyle.totalCoinAmount}>
+            {type === 'single-coin'
+              ? `${formatter.crypto(coin.coinAmount)} ${coin.short}`
+              : 'TOTAL AMOUNT'
+            }
+          </Text>
+        </View>
         <TouchableOpacity style={{position: 'absolute', top: 0, bottom: 0, left: 15, opacity: .4, justifyContent: 'center', alignItems: 'center'}} onPress={this.props.onPressPrevious}>
           <Icon name="IconChevronLeft" height='20' width='20' fill="white" viewBox="0 0 22 22" />
         </TouchableOpacity>
@@ -33,14 +63,11 @@ class WalletDetailsHeading extends Component {
           <Icon name="IconChevronRight" height='20' width='20' fill="white" viewBox="0 0 22 22" />
         </TouchableOpacity>
       </View>
-      <View style={WalletDetailsHeadingStyle.buttonWrapper}>
+      {type === 'single-coin' && <View style={WalletDetailsHeadingStyle.buttonWrapper}>
         <CelButton size="mini" white onPress={() => {this.props.navigateTo('AddFunds')}}>Add funds</CelButton>
-      </View>
+      </View>}
     </View>
   }
 }
 
 export default WalletDetailsHeading;
-
-
-// todo - styles
