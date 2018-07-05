883281fb9cb9e050b0e21db7947eb6b0ba39d4c8
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-07-05 18:34:44 +0200

    CN-690, CN-723: upgrading phone input

--- app/components/molecules/CelPhoneInput/CelPhoneInput.js
+++ app/components/molecules/CelPhoneInput/CelPhoneInput.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import { Item, Label } from "native-base";
 import PhoneInput from 'react-native-phone-input';
-import get from 'lodash/get';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
@@ -51,6 +50,21 @@ class CelPhoneInput extends Component {
     });
   }
 
+  componentDidUpdate() {
+    const { value } = this.props;
+    // format '+000 000 000 000 000'
+    if (value) {
+      let phoneDisplay = [];
+      if (value.substring(0,4)) phoneDisplay.push(value.substring(0,4));
+      if (value.substring(4,7)) phoneDisplay.push(value.substring(4,7));
+      if (value.substring(7,10)) phoneDisplay.push(value.substring(7,10));
+      if (value.substring(10,13)) phoneDisplay.push(value.substring(10,13));
+      if (value.substring(13,16)) phoneDisplay.push(value.substring(13,16));
+      phoneDisplay = phoneDisplay.join(' ');
+      this.phone.inputPhone.setNativeProps({ text: phoneDisplay })
+    }
+  }
+
   onPressFlag() {
     if (this.props.editable) {
       this.setState({ showCountryModal: true });
@@ -61,32 +75,54 @@ class CelPhoneInput extends Component {
     const countryAlpha2 = country != null ? country.alpha2.toLowerCase() : 'US';
     this.phone.selectCountry(countryAlpha2);
     this.setState({ alpha2: countryAlpha2, showCountryModal: false });
+    this.changePhoneNumber(country.countryCallingCodes[0]);
   }
 
+  changePhoneNumber = (text) => {
+    const { updateFormField, field } = this.props;
+    updateFormField(field, text.split(' ').join(''));
+  }
+
+  saveLayout = () => {
+    const { setInputLayout, field } = this.props;
+    this.phone.inputPhone.measureInWindow((x, y, width, height) => {
+      setInputLayout(field, { x, y, width, height });
+    })
+  }
+
+  focusPhoneInput = () => {
+    this.props.scrollTo({ field: this.props.field });
+    return false;
+  }
 
   render() {
-    const { theme, updateFormField, field, value } = this.props;
+    const { theme, value } = this.props;
     const { showCountryModal } = this.state;
     const disabled = this.props.editable === false;
 
-    const labelStyles = {...globalStyles.selectLabelInactive,  position: 'absolute', left: 40, ...globalStyles[`${theme}InputTextColor`]};
+    const labelStyles = [globalStyles.selectLabelActive];
+    labelStyles.push(globalStyles[`${theme}InputTextColor`])
     const inputStyles = {...globalStyles.input, ...globalStyles[`${theme}InputTextColor`]}
-    const inputPhoneValue = get(this.phone, 'inputPhone.props.value', null);
-
     const phoneBackground = value ? globalStyles[`${theme}InputWrapperActive`] : globalStyles[`${theme}InputWrapper`];
 
+    const phoneInputProps = {
+      maxLength: 16 + 4,
+      onFocus: this.focusPhoneInput,
+      onLayout: this.saveLayout,
+    }
+
     return (
-      <View style={[globalStyles.inputWrapper, globalStyles[`${theme}InputWrapper`], phoneBackground]}>
+      <View
+        style={[globalStyles.inputWrapper, globalStyles[`${theme}InputWrapper`], phoneBackground]}
+      >
+        <Label style={labelStyles}>{this.props.labelText.toUpperCase() || 'PHONE'}</Label>
         <Item style={globalStyles.inputItem}>
-          {!inputPhoneValue ? <Label style={labelStyles}>{this.props.labelText || ''}</Label> : null}
           <PhoneInput
-            ref={(ref) => {
-              this.phone = ref;
-            }}
+            ref={(ref) => { this.phone = ref }}
             textStyle={inputStyles}
+            textProps={phoneInputProps}
             onPressFlag={this.onPressFlag}
-            onChangePhoneNumber={(text) => updateFormField(field, text)}
-            value={this.props.value}
+            onChangePhoneNumber={this.changePhoneNumber}
             disabled={disabled}
           />
           <SelectCountryModal
@@ -94,7 +130,6 @@ class CelPhoneInput extends Component {
             visible={ showCountryModal }
             onClose={ country => this.selectCountry(country) }
           />
-          <View />
         </Item>
       </View>
     );
