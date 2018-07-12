9e14f614e0448814e9fc9b4023e84dfc1276e15a
Miljan Tekic <miljan.tekic@gmail.com>
2018-07-12 13:43:12 +0200

    Implemented hooks for bottom navigation for basic layout

--- app/components/layouts/BasicLayout/BasicLayout.js
+++ app/components/layouts/BasicLayout/BasicLayout.js
@@ -7,10 +7,34 @@ import * as actions from "../../../redux/actions";
 @connect(
   state => ({
     bottomNavigationDimensions: state.ui.dimensions.bottomNavigation,
+    activeScreen: state.nav.routes[state.nav.index].routeName,
   }),
   dispatch => bindActionCreators(actions, dispatch),
 )
 class BasicLayout extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      screen: props.activeScreen,
+    }
+  }
+
+  componentDidMount() {
+    const {bottomNavigation, displayBottomNavigation} = this.props;
+
+    displayBottomNavigation(!!bottomNavigation);
+  }
+
+  componentWillReceiveProps({ activeScreen }) {
+    const {bottomNavigation, displayBottomNavigation} = this.props;
+    const {screen} = this.state;
+
+    if (activeScreen === screen && activeScreen !== this.props.activeScreen) {
+      displayBottomNavigation(!(bottomNavigation === false));
+    }
+  }
+
   render() {
     const { bottomNavigation, bottomNavigationDimensions } = this.props;
     let marginBottom;
