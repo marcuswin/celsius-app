a76571493513b81f0dbdd5a04bf569445617d2a6
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-08-09 17:19:19 +0200

    CN-1076: Create WalletLanding layout

---
+++ app/components/layouts/WalletLayout/WalletLayout.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+import { Content } from 'native-base';
+import {connect} from 'react-redux';
+import {bindActionCreators} from "redux";
+import get from "lodash/get";
+
+import * as appActions from "../../../redux/actions";
+// import {STYLES} from "../../config/constants/style";
+import WalletLayoutStyle from "./WalletLayout.styles";
+import BasicLayout from "../BasicLayout/BasicLayout";
+import { MainHeader } from "../../molecules/MainHeader/MainHeader";
+import TabNavigation from "../../molecules/TabNavigation/TabNavigation";
+import formatter from "../../../utils/formatter";
+
+@connect(
+  state => ({
+    walletTotal: state.wallet.total,
+  }),
+  dispatch => ({ actions: bindActionCreators(appActions, dispatch) }),
+)
+class WalletLayout extends Component {
+  // lifecycle methods
+  componentDidMount() {
+    this.props.actions.displayBottomNavigation(true);
+  }
+
+  tabs = [
+    { label: 'Balance', screen: 'Home' },
+    { label: 'Transactions', screen: 'WalletTransactions' },
+    // { label: 'Interest', screen: 'WalletInterest' },
+  ]
+
+  // rendering methods
+  render() {
+    const { walletTotal } = this.props;
+    const total = get(walletTotal, 'quotes.USD.total', 0)
+    return (
+      <BasicLayout>
+        <MainHeader backButton={false} />
+        <View style={WalletLayoutStyle.heading}>
+          <Text style={WalletLayoutStyle.amountText}>{ formatter.usd(total) }</Text>
+          <Text style={WalletLayoutStyle.subheadingText}>WALLET BALANCE</Text>
+        </View>
+        <TabNavigation tabs={this.tabs}/>
+
+        <Content style={WalletLayoutStyle.content}>
+          { this.props.children }
+        </Content>
+      </BasicLayout>
+    );
+  }
+}
+
+export default WalletLayout;
