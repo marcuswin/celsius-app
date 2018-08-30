799353bfe5dabc8cfe7fa89f2b372a959d3f0ca0
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-08-30 17:01:38 +0200

    CN-570: fixing scroll bug

--- app/components/atoms/CelModal/CelModal.js
+++ app/components/atoms/CelModal/CelModal.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { View, Modal, TouchableOpacity} from 'react-native';
+import { View, Modal, TouchableOpacity, ScrollView } from 'react-native';
 import {connect} from 'react-redux';
 import {bindActionCreators} from "redux";
 
@@ -39,9 +39,9 @@ class CelModal extends Component {
             <TouchableOpacity style={CelModalStyle.closeBtn} onPress={this.closeModal}>
               <Icon name='xIcon' height='20' width='20' viewBox="0 0 1000 1000" fill={'#3D4853'}/>
             </TouchableOpacity>
-            <View>
+            <ScrollView>
               { this.props.children }
-            </View>
+            </ScrollView>
           </View>
         </View>
       </Modal>
