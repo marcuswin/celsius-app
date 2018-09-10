0dc56f58a3a64fadc57307d7a13002b9750aaa5b
Nikola Sabelnik <sabelnikola@gmail.com>
2018-09-10 14:12:30 +0200

    CN-1184 Add funds > Security modal - Change copy and add BitGo logo

--- app/components/screens/SecureTransactions/SecureTransactions.js
+++ app/components/screens/SecureTransactions/SecureTransactions.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {connect} from 'react-redux';
-import {bindActionCreators} from "redux";
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 import * as appActions from "../../../redux/actions";
 import Icon from "../../atoms/Icon/Icon";
 import CelButton from "../../atoms/CelButton/CelButton";
 import SimpleLayout from "../../layouts/SimpleLayout/SimpleLayout";
-import {STYLES} from "../../../config/constants/style";
+import { STYLES } from "../../../config/constants/style";
 import SecureTransactionsStyle from "./SecureTransactions.styles";
 
 @connect(
   state => ({
-  // map state to props
+    // map state to props
     nav: state.nav
   }),
-  dispatch => ({ actions: bindActionCreators(appActions, dispatch) }),
+  dispatch => ({ actions: bindActionCreators(appActions, dispatch) })
 )
 class SecureTransactions extends Component {
   constructor(props) {
@@ -31,13 +31,14 @@ class SecureTransactions extends Component {
     const { actions } = this.props;
     actions.updateUserAppSettings({ showSecureTransactionsScreen: false });
   }
+
   // event hanlders
   // rendering methods
   render() {
-    const {actions, navigation} = this.props;
-    const currency = navigation.getParam('currency')
+    const { actions, navigation } = this.props;
+    const currency = navigation.getParam("currency");
 
-    const currencyCopy = currency ? currency.toUpperCase() : 'coins'
+    const currencyCopy = currency ? currency.toUpperCase() : "coins";
     return (
 
       <SimpleLayout
@@ -45,29 +46,38 @@ class SecureTransactions extends Component {
         bottomNavigation={false}
       >
         <View style={SecureTransactionsStyle.content}>
-        <Icon  name={ 'Shield' }
-               width='65'
-                 height='65'
-               fill={ 'white'}
-               stroke={'white'}
-               style={{
-                 marginBottom: 15,
-                 opacity: 0.5,
-               }}
-        />
-        <Text style={SecureTransactionsStyle.title}>Secure transactions</Text>
-        <Text style={SecureTransactionsStyle.explanation}>Please keep in mind that you’ll only be able to withdraw {currencyCopy} to the original wallet you sent us {currencyCopy} from, but anyone can send {currencyCopy} to the address you initially set.</Text>
-        <View style={SecureTransactionsStyle.suggestionWrapper}>
-          <Text style={SecureTransactionsStyle.suggestion}>For your security, if you would like to withdraw more than <Text style={{fontFamily: 'agile-bold'}}>$20.000</Text> worth of {currencyCopy} you will be required to contact us at <Text style={{fontFamily: 'agile-bold'}}>app@celsius.network</Text> so that we can verify your identity prior to transferring your funds.</Text>
-        </View>
-          <CelButton
-            onPress={() => actions.navigateTo('AddFunds', { currency })}
-            white
-          >
-            Add Funds
-          </CelButton>
 
+          <Icon name={"BitGo"}
+                width='199'
+                height='50'
+                fill={"white"}
+                stroke={"white"}
+                style={{
+                  marginBottom: 15
+                }}
+          />
         </View>
+        <Text style={SecureTransactionsStyle.title}>ALL TRANSACTIONS ARE SAFELY SECURED AND DEPOSITED WITH
+          BITGO</Text>
+        <Text style={SecureTransactionsStyle.explanation}>BitGo is a leading custodial service powering exchanges like
+          Kraken and UPbit. Coins will also be moved from time to time to exchanges or to Hedge funds borrowing coins
+          in order to short the market, in this case, coins are converted to fiat and secured in an FDIC-insured
+          bank account.</Text>
+
+        <CelButton
+          onPress={() => actions.navigateTo("AddFunds", { currency })}
+          white
+        >
+          Add Funds
+        </CelButton>
+
+        <View style={SecureTransactionsStyle.suggestionWrapper}>
+          <Text style={SecureTransactionsStyle.suggestion}>For your security, if you would like to withdraw more
+            than <Text style={{ fontFamily: "agile-bold" }}>$20.000</Text> worth of {currencyCopy} you will be
+            required to contact us at <Text style={{ fontFamily: "agile-bold" }}>app@celsius.network</Text> so that we
+            can verify your identity prior to transferring your funds.</Text>
+        </View>
+
       </SimpleLayout>
 
     );
