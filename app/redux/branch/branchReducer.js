d8fae857f8c419e61fca045a1002b179a9298340
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-09-12 11:12:43 +0200

    CN-250: updating reducers

--- app/redux/branch/branchReducer.js
+++ app/redux/branch/branchReducer.js
@@ -1,12 +1,18 @@
 import ACTIONS from "../../config/constants/ACTIONS";
 
 const initialState = {
+  allLinks: [],
   initialized: false,
   referralObject: null
 };
 
 export default function branchReducer(state = initialState, action) {
   switch (action.type) {
+    case ACTIONS.BRANCH_LINK_REGISTERED:
+      return {
+        ...state,
+        allLinks: [ ...state.allLinks, action.link ],
+      };
     case ACTIONS.BRANCH_LINK_INITIALIZED:
       return {
         ...state,
--- app/redux/transfers/transfersReducer.js
+++ app/redux/transfers/transfersReducer.js
@@ -1,4 +1,5 @@
 import ACTIONS from '../../config/constants/ACTIONS';
+import { BRANCH_LINKS } from '../../config/constants/common';
 
 function initialState() {
     return {
@@ -10,12 +11,13 @@ export default function transfersReducer(state = initialState(), action) {
 
     switch (action.type) {
       case ACTIONS.BRANCH_LINK_REGISTERED:
-        if (action.link.link_type === 'TRANSFER_RECEIVED') {
+        if (action.link.link_type === BRANCH_LINKS.TRANSFER_RECEIVED) {
           return {
             ...state,
             branchHashes: [ ...state.branchHashes, action.link.transfer_hash ]
           };
         }
+        break;
 
     default:
         return { ...state };
