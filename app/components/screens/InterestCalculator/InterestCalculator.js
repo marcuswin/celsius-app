2e0be6bd3a3e3bc3cc1fbb0a2f01edf17ec68800
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-09-26 17:58:21 +0200

    CN-1272: fixing interest rates

--- app/components/screens/InterestCalculator/InterestCalculator.js
+++ app/components/screens/InterestCalculator/InterestCalculator.js
@@ -27,6 +27,8 @@ import { KYC_STATUSES } from "../../../config/constants/common";
     formData: state.ui.formData,
     user: state.users.user,
     interestRates: state.interest.rates,
+    interestRatesDisplay: state.interest.ratesDisplay,
+    supportedCurrencies: state.generalData.supportedCurrencies
   }),
   dispatch => ({ actions: bindActionCreators(appActions, dispatch) }),
 )
@@ -44,7 +46,7 @@ class InterestCalculatorScreen extends Component {
   }
 
   render() {
-    const { formData, interestRates, actions, user } = this.props;
+    const { formData, interestRates, interestRatesDisplay, actions, user } = this.props;
 
     if (!interestRates) return (
       <EarnInterestLayout>
@@ -52,7 +54,7 @@ class InterestCalculatorScreen extends Component {
       </EarnInterestLayout>
     )
 
-    const displayInterestRate = `${interestRates[formData.interestCurrency] * 100}%`;
+    const displayInterestRate = `${interestRatesDisplay[formData.interestCurrency]}%`;
     const interest = formData.interestAmount * interestRates[formData.interestCurrency];
     const interestPerWeek = interest / 52;
     const interestPerMonth = interest / 12;
