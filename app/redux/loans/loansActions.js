bf4e7a8d848cb788fdc3af264d3f279aceb6d7af
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-10-29 15:59:51 +0100

    CN-1440: loan amount added

--- app/redux/loans/loansActions.js
+++ app/redux/loans/loansActions.js
@@ -21,7 +21,7 @@ function applyForALoan() {
         collateral_amount_crypto: formData.amountCollateralCrypto,
         ltv: formData.ltv.percent,
         interest: formData.ltv.interest,
-        loan_amount_usd: formData.loanAmount,
+        loan_amount: formData.loanAmount,
         monthly_payment: formData.monthlyPayment || 10,
       }
 
