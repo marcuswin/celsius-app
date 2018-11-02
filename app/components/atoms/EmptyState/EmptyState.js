75e87604b2bd6f10b7fce8917f0e2a4b190e9603
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-11-02 01:01:51 +0100

    CN-1462: fix android CelPay

--- app/components/atoms/EmptyState/EmptyState.js
+++ app/components/atoms/EmptyState/EmptyState.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, Platform } from "react-native";
+import { View, Text, Image } from "react-native";
 import EmptyStateStyle from "./EmptyState.styles";
 import { GLOBAL_STYLE_DEFINITIONS as globalStyles } from "../../../config/constants/style";
 
@@ -12,9 +12,7 @@ const EmptyState = (props) => {
   switch (props.screen) {
     case "SelectCoin":
       title = "From here you can pay your friends back with crypto";
-      text = Platform.os === 'ios' ?
-        "To send your favorite coins to your favorite people, please verify your identity first." :
-        "This feature is coming to Android soon!";
+      text = "To send your favorite coins to your favorite people, please verify your identity first.";
       break;
     default:
       title = "From here you can pay your friends back with crypto";
