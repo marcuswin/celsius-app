64127d09ebb7438520561beb819bba1ac0df36cc
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-12-03 15:11:04 +0100

    CN-1634: adding tests

--- e2e-tests/index.js
+++ e2e-tests/index.js
@@ -1,4 +1,5 @@
 import signupFlow from './flows/signup-flow';
+import helpers from './helpers';
 
 export default function(spec) {
   testSignupFlow(spec);
@@ -24,16 +25,18 @@ export function testSignupFlow(spec) {
     spec.it('button should be disabled when no email and password entered', signupFlow.disableWhenNoData(spec))
     spec.it('button should be disabled when no email entered', signupFlow.disableWhenNoEmail(spec))
     spec.it('button should be disabled when no password entered', signupFlow.disableWhenNoPassword(spec))
+    spec.it('button should be disabled when no repeat password entered', helpers.testFailed(spec))
     spec.it('should show error when invalid email entered', signupFlow.errorWhenEmailInvalid(spec))
     spec.it('should show error when when weak password', signupFlow.errorWhenPasswordWeak(spec))
     spec.it('should show error when user exists', signupFlow.errorWhenUserExists(spec))
+    spec.it('button should be disabled when repeat password not same as password', helpers.testFailed(spec))
     spec.it('should go to SignupTwo screen when all info is valid', signupFlow.stepOneSuccess(spec))
 
     // SignupTwo screen tests
     spec.it('button should be disabled when no first and last name entered', signupFlow.disableWhenNoNames(spec))
     spec.it('button should be disabled when no first name entered', signupFlow.disableWhenNoLastName(spec))
     spec.it('button should be disabled when no last name entered', signupFlow.disableWhenNoFirstName(spec))
-    spec.it('button should be disabled when terms not agreed to', signupFlow.disableWhenNoFirstName(spec))
+    spec.it('button should be disabled when terms not agreed to', helpers.testFailed(spec))
     spec.it('should go to EnterPasscode screen when all info is valid', signupFlow.stepTwoSuccess(spec))
 
     // CreatePasscode screen tests
@@ -44,10 +47,5 @@ export function testSignupFlow(spec) {
     spec.it('should disable button click when 3 digits are entered', signupFlow.disableRepeatPasscode(spec))
     spec.it('should show error when different pin is entered', signupFlow.disableWrongPasscode(spec))
     spec.it('should go to NoKYC screen when repeated pin is valid', signupFlow.finishPasscode(spec))
-
-    // // User exited registration process after first step
-    // spec.it('should go to SignupTwo screen on app open if user hasn\'t entered his name', testPassed(spec))
-    // spec.it('should go to EnterPasscode screen on app open if user hasn\'t entered his pin', testPassed(spec))
-    // spec.it('should go to NoKYC screen when all steps are successfully finished', testPassed(spec))
   })
 }
