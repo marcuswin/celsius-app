dbf2f12b854802b02ec4495ef1c03c82d2ae0220
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-12-31 11:04:03 +0100

    CN-1754: upgrade screen

--- app/components/screens/BrwEnterAmount/BrwEnterAmount.js
+++ app/components/screens/BrwEnterAmount/BrwEnterAmount.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, Slider } from "react-native";
+import { Text } from "react-native";
 import {connect} from 'react-redux';
 import {bindActionCreators} from "redux";
 
@@ -11,6 +11,10 @@ import CelForm from "../../atoms/CelForm/CelForm";
 import { GLOBAL_STYLE_DEFINITIONS as globalStyles } from "../../../config/constants/style";
 import { LOAN_ELIGIBLE_COINS } from "../../../config/constants/common";
 import Steps from "../../molecules/Steps/Steps";
+import CelSlider from "../../molecules/CelSlider/CelSlider";
+import formatter from '../../../utils/formatter';
+
+const MIN_LOAN_AMOUNT = 10000;
 
 @connect(
   state => ({
@@ -22,9 +26,28 @@ import Steps from "../../molecules/Steps/Steps";
 )
 
 class BRWEnterAmount extends Component {
+  constructor(props) {
+    super(props)
+
+    const { eligibleCurrencies } = props;
+    this.state = {
+      maxAmount: eligibleCurrencies.reduce((max, element) => element.total > max ? element.total : max, 0) / 2,
+    }
+  }
+  submitAmount = () => {
+    const { formData, actions } = this.props;
+    const { maxAmount } = this.state;
+
+    if (!formData.amount) return actions.showMessage('error', 'Please enter an amount you wish to borrow')
+    if (Number(formData.amount) < MIN_LOAN_AMOUNT) return actions.showMessage('warning', `Minimum loan starts at ${ formatter.usd(MIN_LOAN_AMOUNT) }`)
+    if (Number(formData.amount) > maxAmount) return actions.showMessage('warning', 'Insufficient funds')
+
+    actions.navigateTo('BRWChooseCollateral')
+  }
+
   render() {
-    const { formData, actions, eligibleCurrencies } = this.props;
-    const maxAmount = eligibleCurrencies.reduce((max, element) => element.amount > max ? element.amount : max, 0) / 2;
+    const { formData } = this.props;
+    const { maxAmount } = this.state;
 
     return (
       <SimpleLayout
@@ -32,31 +55,30 @@ class BRWEnterAmount extends Component {
       >
         <Steps current={1} totalSteps={5} />
         <CelForm margin="20 0 0 0">
-          <Text style={globalStyles.normalText}>How much do you want to borrow?</Text>
-
-          <Text style={globalStyles.normalText}>Max amount: { maxAmount }</Text>
+          <Text style={[globalStyles.normalText, { marginVertical: 15 }]}>How much do you want to borrow?</Text>
 
           <CelInput
             theme="white"
             type="number"
             labelText="$10,000"
             value={formData.amount ? formData.amount.toString() : ''}
-            field="currentPassword"
+            field="amount"
           />
 
-          <Slider
-            minimumValue={5000}
-            maximumValue={50000}
+          <CelSlider
+            minimumValue={MIN_LOAN_AMOUNT}
+            maximumValue={maxAmount}
+            step={100}
             value={formData.amount}
-            step={1}
-            onValueChange={(value) => actions.updateFormField('amount', value)}
+            field={'amount'}
+            items={[{ label: formatter.usd(MIN_LOAN_AMOUNT) }, { label: formatter.usd(maxAmount) }]}
           />
         </CelForm>
 
         <CelButton
           margin="40 0 0 0"
           color="blue"
-          onPress={() => actions.navigateTo('BRWChooseCollateral')}
+          onPress={this.submitAmount}
         >
           Choose a collateral
         </CelButton>
