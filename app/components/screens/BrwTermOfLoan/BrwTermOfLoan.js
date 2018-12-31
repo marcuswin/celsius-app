bc942f7518bc0d17ce828bd9283f021b80b5e827
Filip Jovakaric <filip.jovakaric@gmail.com>
2018-12-31 11:09:52 +0100

    CN-1757: upgrade screen

--- app/components/screens/BrwTermOfLoan/BrwTermOfLoan.js
+++ app/components/screens/BrwTermOfLoan/BrwTermOfLoan.js
@@ -6,9 +6,9 @@ import {bindActionCreators} from "redux";
 import * as appActions from "../../../redux/actions";
 import SimpleLayout from "../../layouts/SimpleLayout/SimpleLayout";
 import CelButton from '../../atoms/CelButton/CelButton';
-import CelForm from "../../atoms/CelForm/CelForm";
 import { GLOBAL_STYLE_DEFINITIONS as globalStyles } from "../../../config/constants/style";
 import Steps from "../../molecules/Steps/Steps";
+import CelSlider from "../../molecules/CelSlider/CelSlider";
 
 @connect(
   state => ({
@@ -20,15 +20,31 @@ import Steps from "../../molecules/Steps/Steps";
 
 class BRWTermOfLoan extends Component {
   render() {
-    const { actions } = this.props;
+    const { actions, formData } = this.props;
+    const items = [
+      { label: '6 M' },
+      { label: '12 M' },
+      { label: '18 M' },
+      { label: '24 M' },
+      { label: '30 M' },
+      { label: '36 M' },
+    ]
     return (
       <SimpleLayout
         animatedHeading={{ text: 'Term of loan'}}
       >
         <Steps current={4} totalSteps={5} />
-        <CelForm margin="20 0 0 0">
-          <Text style={globalStyles.normalText}>Select for how long the loan will last:</Text>
-        </CelForm>
+
+        <Text style={[globalStyles.normalText, { marginVertical: 15 }]}>Select for how long the loan will last:</Text>
+
+        <CelSlider
+          field="termOfLoan"
+          minimumValue={6}
+          maximumValue={36}
+          step={6}
+          value={formData.termOfLoan}
+          items={items}
+        />
 
         <CelButton
           margin="40 0 0 0"
