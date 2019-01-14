ae14aeb4784f28187d5647463e872d44d32bd4a2
StefanBurscher <stefan.burscher@gmail.com>
2019-01-14 11:37:29 +0100

    CN-1797: Locked funds info added on borrow confirmation

--- app/components/screens/BrwConfirmLoan/BrwConfirmLoan.js
+++ app/components/screens/BrwConfirmLoan/BrwConfirmLoan.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import {connect} from 'react-redux';
-import {bindActionCreators} from "redux";
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
 
 import { GLOBAL_STYLE_DEFINITIONS as globalStyles } from "../../../config/constants/style";
 import * as appActions from "../../../redux/actions";
@@ -12,6 +12,7 @@ import Card from "../../atoms/Card/Card";
 import Separator from "../../atoms/Separator/Separator";
 import formatter from '../../../utils/formatter';
 import testUtil from "../../../utils/test-util";
+import InfoBubble from '../../atoms/InfoBubble/InfoBubble';
 
 @connect(
   state => ({
@@ -27,32 +28,44 @@ class BRWTermOfLoan extends Component {
     const monthlyPayment = formData.collateralAmountUSD * formData.ltv.interest / 12;
     return (
       <SimpleLayout
-        animatedHeading={{ text: 'Confirm your loan'}}
+        animatedHeading={{ text: 'Confirm your loan' }}
       >
         <Steps current={5} totalSteps={5} />
 
+        <InfoBubble
+          color={"gray"}
+          margin={"22 0 0 0"}
+          renderContent={() => (
+            <View>
+              <Text style={[globalStyles.normalText, { color: 'white' }]}>
+                Your collateral will be locked and not earning interest until the loan is entirely paid out.
+                </Text>
+            </View>
+          )}
+        />
+
         <Card style={{ padding: 20, marginTop: 20 }}>
           <Text style={globalStyles.normalText}>You are borrowing:</Text>
-          <Text style={[globalStyles.heading, { textAlign: 'left' }]}>{ formatter.usd(formData.amount) } USD</Text>
-          <Separator margin="10 0 10 0"/>
+          <Text style={[globalStyles.heading, { textAlign: 'left' }]}>{formatter.usd(formData.amount)} USD</Text>
+          <Separator margin="10 0 10 0" />
 
           <Text style={globalStyles.normalText}>Collateral:</Text>
-          <Text style={[globalStyles.heading, { textAlign: 'left' }]}>{ formatter.usd(formData.collateralAmountUSD) } USD</Text>
-          <Text style={globalStyles.normalText}>{ formatter.crypto(formData.collateralAmountCrypto, formData.coin, { precision: 5 }) }</Text>
-          <Separator margin="10 0 10 0"/>
+          <Text style={[globalStyles.heading, { textAlign: 'left' }]}>{formatter.usd(formData.collateralAmountUSD)} USD</Text>
+          <Text style={globalStyles.normalText}>{formatter.crypto(formData.collateralAmountCrypto, formData.coin, { precision: 5 })}</Text>
+          <Separator margin="10 0 10 0" />
 
           <Text style={globalStyles.normalText}>Term of loan:</Text>
-          <Text style={[globalStyles.heading, { textAlign: 'left' }]}>{ formData.termOfLoan } months</Text>
-          <Separator margin="10 0 10 0"/>
+          <Text style={[globalStyles.heading, { textAlign: 'left' }]}>{formData.termOfLoan} months</Text>
+          <Separator margin="10 0 10 0" />
 
           <View style={{ flexDirection: 'row' }}>
-            <View style={{ width: '50%', padding: 10, justifyContent: 'center', alignItems: 'center'}}>
-              <Text style={[globalStyles.heading, {textAlign: 'center'}]}>{ formData.ltv.interest * 100 }%</Text>
-              <Text style={[globalStyles.normalText, {textAlign: 'center'}]}>Annual interest rate</Text>
+            <View style={{ width: '50%', padding: 10, justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={[globalStyles.heading, { textAlign: 'center' }]}>{formData.ltv.interest * 100}%</Text>
+              <Text style={[globalStyles.normalText, { textAlign: 'center' }]}>Annual interest rate</Text>
             </View>
-            <View style={{ width: '50%', padding: 10, justifyContent: 'center', alignItems: 'center'}}>
-              <Text style={[globalStyles.heading, {textAlign: 'center'}]}>{ formatter.usd(monthlyPayment) }</Text>
-              <Text style={[globalStyles.normalText, {textAlign: 'center'}]}>Monthly interest payment</Text>
+            <View style={{ width: '50%', padding: 10, justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={[globalStyles.heading, { textAlign: 'center' }]}>{formatter.usd(monthlyPayment)}</Text>
+              <Text style={[globalStyles.normalText, { textAlign: 'center' }]}>Monthly interest payment</Text>
             </View>
           </View>
         </Card>
