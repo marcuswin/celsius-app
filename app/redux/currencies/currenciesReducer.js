0e66f5252f1f3d94488ea896d81144cdde0bb0f3
Filip Jovakaric <filip.jovakaric@gmail.com>
2019-02-07 19:35:59 +0100

    CN-1855: adding currencyRatesShort

--- app/redux/currencies/currenciesReducer.js
+++ app/redux/currencies/currenciesReducer.js
@@ -1,25 +1,32 @@
-import ACTIONS from '../../constants/ACTIONS';
+import ACTIONS from "../../constants/ACTIONS";
 
 const initialState = {
   rates: undefined,
   graphs: undefined,
-}
+  currencyRatesShort: undefined,
+};
 
 export default function currenciesReducer(state = initialState, action) {
+  let currencyRatesShort;
 
-    switch (action.type) {
+  switch (action.type) {
     case ACTIONS.GET_CURRENCY_RATES_SUCCESS:
-        return {
-          ...state,
-          rates: action.rates,
-        };
+      action.rates.forEach(sc => {
+        currencyRatesShort[sc.short.toLowerCase()] = sc.market_quotes_usd.price;
+      });
+
+      return {
+        ...state,
+        rates: action.rates,
+        currencyRatesShort,
+      };
     case ACTIONS.GET_CURRENCY_GRAPHS_SUCCESS:
-        return {
-          ...state,
-          graphs: action.graphs,
-        };
+      return {
+        ...state,
+        graphs: action.graphs
+      };
 
     default:
-        return { ...state };
-    }
+      return { ...state };
+  }
 }
