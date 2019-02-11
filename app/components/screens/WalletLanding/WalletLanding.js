b8e65208c2998b7c3a817121425a48272d49b422
Filip Jovakaric <filip.jovakaric@gmail.com>
2019-02-11 13:24:27 +0100

    CN-1879: fixing screen styles

--- app/components/screens/WalletLanding/WalletLanding.js
+++ app/components/screens/WalletLanding/WalletLanding.js
@@ -13,6 +13,7 @@ import CelText from "../../atoms/CelText/CelText";
 import Card from "../../atoms/Card/Card";
 import STYLES from "../../../constants/STYLES";
 import CoinCard from '../../molecules/CoinCard/CoinCard';
+import Separator from "../../atoms/Separator/Separator";
 
 @connect(
   state => ({
@@ -51,21 +52,19 @@ class WalletLanding extends Component {
       >
         <View>
           <Card>
-            <View style={{ flexDirection: "row" }}>
+            <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+
               <TouchableOpacity style={[walletStyle.balance]} onPress={() => actions.navigateTo('BalanceHistory')}>
-                <CelText style={walletStyle.text}>Total Wallet balance</CelText>
-                <CelText bold style={walletStyle.usd}>{formatter.usd(walletSummary.total_amount_usd)}</CelText>
+                <CelText type="H6" color="color: rgba(61,72,83,0.7)">Total Wallet balance</CelText>
+                <CelText type="H3" bold>{formatter.usd(walletSummary.total_amount_usd)}</CelText>
                 <CelText color={STYLES.COLORS.RED}>{walletSummary.wallet_diff_24h}</CelText>
               </TouchableOpacity>
-              <View style={{
-                marginRight: 10,
-                borderLeftWidth: 1,
-                borderLeftColor: '#F3F3F3',
-                borderRadius: 10,
-              }} />
+
+              <Separator vertical />
+
               <TouchableOpacity style={walletStyle.interest} onPress={() => actions.navigateTo('WalletInterest')}>
-                <CelText style={walletStyle.text}>Total Interest earned</CelText>
-                <CelText bold style={walletStyle.usd}>{formatter.usd(walletSummary.total_interest_earned)}</CelText>
+                <CelText type="H6" color="color: rgba(61,72,83,0.7)">Total Interest earned</CelText>
+                <CelText type="H3" bold>{formatter.usd(walletSummary.total_interest_earned)}</CelText>
                 <CelText color={STYLES.COLORS.CELSIUS_BLUE}>Todays rates</CelText>
               </TouchableOpacity>
             </View>
