af6a7c512a6f768844d44023948f8a6af0f80253
StefanBurscher <stefan.burscher@gmail.com>
2019-02-14 15:41:29 +0100

    Error handling and sending to graylog created

--- app/utils/logger-util.js
+++ app/utils/logger-util.js
@@ -1,5 +1,7 @@
 import axios from "axios/index";
 import { Constants } from "expo";
+import apiUrl from "../services/api-url";
+import store from "../redux/store";
 
 const { ENV } = Constants.manifest.extra;
 
@@ -8,6 +10,7 @@ export default {
   log,
   warn,
   info,
+  err
 }
 
 function log(content) {
@@ -24,4 +27,33 @@ function warn(content) {
 
 function logme(payload) {
   axios.post('https://api.staging.celsius.network/api/v1/logme', payload);
+}
+
+function err(e, isFatal = false) {
+
+  const { revisionId } = Constants.manifest;
+
+  const state = store.getState();
+  const { user } = state.users;
+  const userData = user && { user_id: user.id, email: user.email };
+
+  const { nav } = state;
+  const activeScreen = nav.routes[nav.index].routeName;
+
+  const error = {
+    name: e.name,
+    stack: e.stack,
+    message: e.message,
+  }
+
+  const errorObject = {
+    err: error,
+    user: userData,
+    active_screen: activeScreen,
+    is_fatal: isFatal && (typeof isFatal === 'string') && isFatal === 'true',
+    app_version: revisionId,
+    platform: Constants.platform
+  }
+
+  axios.post(`${apiUrl}/graylog`, errorObject)
 }
