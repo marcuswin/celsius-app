0ddb229c7700d8f4d306f10c551b88110cd00b28
Nikola Sabelnik <sabelnikola@gmail.com>
2019-02-15 14:57:26 +0100

    CN-1928 As a user ISBAT check my withdrawal address

--- app/components/atoms/InfoBox/InfoBox.js
+++ app/components/atoms/InfoBox/InfoBox.js
@@ -1,34 +1,116 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Text, View, TouchableOpacity } from "react-native";
 
 import Icon from "../Icon/Icon";
-import Card from "../Card/Card";
 import testUtil from "../../../utils/test-util";
 import { THEMES } from "../../../constants/UI";
 // import InfoBoxStyle from "./InfoBox.styles";
-import STYLES from "../../../constants/STYLES";
 
-const InfoBox = (props) => (
-  <Card { ...props }>
-    <View style={{ flexDirection: "row", alignItems: "flex-start", justifyContent: "space-between" }}>
-      <View>
-        { props.children }
-      </View>
-      <View>
-        <Icon name="WarningCircle" width="23" height="23" fill={STYLES.COLORS.ORANGE} />
-      </View>
-    </View>
-  </Card>
-);
+import { heightPercentageToDP, widthPercentageToDP } from "../../../utils/styles-util";
+import CelText from "../CelText/CelText";
 
-InfoBox.propTypes = {
-  margin: PropTypes.string,
-  padding: PropTypes.string,
-  color: PropTypes.string,
-  onPress: PropTypes.func,
-  theme: PropTypes.oneOf(Object.values(THEMES)),
-  size: PropTypes.oneOf(['full', 'half'])
-}
+class InfoBox extends Component {
+
+  static PropTypes = {
+    margin: PropTypes.string,
+    padding: PropTypes.string,
+    color: PropTypes.string,
+    onPress: PropTypes.func,
+    theme: PropTypes.oneOf(Object.values(THEMES)),
+    size: PropTypes.oneOf(["full", "half"]),
+    titleText: PropTypes.string,
+    infoText: PropTypes.string,
+    right: PropTypes.bool,
+    left: PropTypes.bool,
+    backgroundColor: PropTypes.string,
+    triangle: PropTypes.bool,
+    opened: PropTypes.bool,
+    explanationText: PropTypes.string,
+  };
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  renderLargeInfoBox() {
+    const { open } = this.state;
+    const o = !open;
+    this.setState({
+      open: o
+    });
+  }
+
+  render() {
+    const { left, right, triangle, children, titleText, color, explanationText, backgroundColor } = this.props;
+    const { open } = this.state;
+
+    const rotate = open ? "0deg" : "180deg";
+
+    return (
+      <TouchableOpacity style={{
+        width: "100%",
+        padding: 20,
+        backgroundColor,
+        borderRadius: 8, marginTop: 10, marginBottom: 10
+      }} onPress={() => this.renderLargeInfoBox()}>
+        <View style={{
+          flexDirection: "row", justifyContent: "space-around",
+          alignItems: "center"
+        }}>
+          {left &&
+          <View style={{ paddingRight: 20 }}>
+            <Icon name="WarningCircle" width="23" height="23" stroke={backgroundColor} fill={color}/>
+          </View>
+          }
+          <Text type={"H4"} style={{ color }}>{titleText}</Text>
+          <View>
+            {children}
+          </View>
+          {right &&
+          <View style={{ paddingLeft: 20 }}>
+            <Icon name="WarningCircle" width="23" height="23" stroke={backgroundColor} fill={color}/>
+          </View>
+          }
+          {triangle &&
+          <View style={{ paddingLeft: 20 }}>
+            <View style={{
+              width: 0,
+              height: 0,
+              backgroundColor: "transparent",
+              borderStyle: "solid",
+              marginTop: heightPercentageToDP("1%"),
+              borderLeftWidth: widthPercentageToDP("1.5%"),
+              borderRightWidth: widthPercentageToDP("1.5%"),
+              borderBottomWidth: widthPercentageToDP("1.5%"),
+              borderLeftColor: "transparent",
+              borderRightColor: "transparent",
+              borderBottomColor: "white",
+              transform: [
+                { rotate }
+              ]
+            }}/>
+          </View>
+          }
+        </View>
+        {open &&
+        <View style={{marginTop: 5, width: "70%", flexDirection: "row", alignItems: "flex-end", height: heightPercentageToDP("19.05%")}}>
+          <View style={{width: "30%"}}/>
+          <CelText type={"H6"} style={{ color }}>{explanationText}</CelText>
+        </View>
+        }
+        }
+      </TouchableOpacity>
+
+    );
+  }
+};
+
 
 export default testUtil.hookComponent(InfoBox);
