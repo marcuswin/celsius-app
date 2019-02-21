a705667537bb964a94e9d67da2c888aab19f79bf
nemanja <nemanjakrstonic2@gmail.com>
2019-02-21 14:11:30 +0100

    CN-1923: fixing BalanceView styles

--- app/components/atoms/BalanceView/BalanceView.js
+++ app/components/atoms/BalanceView/BalanceView.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
 
 import testUtil from "../../../utils/test-util";
-import BalanceViewStyle from "./BalanceView.styles";
+// import BalanceViewStyle from "./BalanceView.styles";
 import CelText from '../../atoms/CelText/CelText';
 import formatter from "../../../utils/formatter";
 import Card from "../Card/Card";
 
 
 const BalanceView = (props) => {
-  const style = BalanceViewStyle();
+  // const style = BalanceViewStyle();
   const {crypto, usd, coin, opacity} = props;
 
   return (
-      <View style={[style.container]}>
-        <Card  padding="10 10 10 10" opacity={opacity}>
-          <CelText align="center" type="H7">
-           New balance: { formatter.crypto(crypto, coin) } | { formatter.usd(usd)  }
-          </CelText>
-        </Card>
-      </View>
+      <Card  padding="10 10 10 10" opacity={opacity}>
+        <CelText align="center" type="H7">
+          New balance: { formatter.crypto(crypto, coin) } | { formatter.usd(usd)  }
+        </CelText>
+      </Card>
     );
 };
 
