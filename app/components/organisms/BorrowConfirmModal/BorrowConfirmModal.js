37b2d80485bb71165b7614aa9a5694b193ddf2bb
Filip Jovakaric <filip.jovakaric@gmail.com>
2019-03-07 12:59:24 +0100

    CN-2043: upgrading modal

--- app/components/organisms/BorrowConfirmModal/BorrowConfirmModal.js
+++ app/components/organisms/BorrowConfirmModal/BorrowConfirmModal.js
@@ -14,54 +14,71 @@ import formatter from "../../../utils/formatter";
 
 const { MODALS } = UI
 
+let fakeFormData = {
+  coin: 'ETH',
+  amountCollateralUSD: 50,
+  amountCollateralCrypto: 0.5,
+  ltv: { percent: 0.33, interest: 0.0695 },
+  loanAmount: '20',
+  termOfLoan: 18,
+  monthlyPayment: 75,
+  bankInfo: { id: 'idofbank'},
+};
+
 const BorrowConfirmModal = ({
   formData,
   onConfirm
-}) => (
-  <CelModal name={MODALS.BORROW_CONFIRM}>
-    <View>
-      <CelText type="H2" weight="bold" align="center" margin="0 0 6 0">Confirm your loan</CelText>
-      <CelText type="H4" weight="300" align="center">By initiating loan you will lock your collateral and initiate a wire transfer of { formatter.usd(formData.loanAmount, { precision: 0 }) } to your bank account.</CelText>
+}) => {
+  fakeFormData = {
+    ...formData,
+    ...fakeFormData,
+  }
+  return (
+    <CelModal name={MODALS.BORROW_CONFIRM}>
+      <View>
+        <CelText type="H2" weight="bold" align="center" margin="0 0 6 0">Confirm your loan</CelText>
+        <CelText type="H4" weight="300" align="center">By initiating loan you will lock your collateral and initiate a wire transfer of { formatter.usd(fakeFormData.loanAmount, { precission: 0 }) } to your bank account.</CelText>
 
-      <Separator margin="22 0 22 0"/>
-      <CelText align="center">You are about to borrow</CelText>
-      <CelText align="center" type="H1" weight="600">{ formatter.usd(formData.loanAmount, { precision: 0 }) }</CelText>
+        <Separator margin="22 0 22 0"/>
+        <CelText align="center">You are about to borrow</CelText>
+        <CelText align="center" type="H1" weight="600">{ formatter.usd(fakeFormData.loanAmount, { precission: 0 }) }</CelText>
 
-      <Separator margin="30 0 16 0"/>
-      <CelText type="H6" weight="300">Collateral</CelText>
-      <CelText type="H6" weight="500">{ formatter.crypto(formData.amountCollateralCrypto, formData.coin) }</CelText>
-      <CelText type="H6" weight="300">{ formatter.usd(formData.amountCollateralUSD) }</CelText>
+        <Separator margin="30 0 16 0"/>
+        <CelText type="H6" weight="300">Collateral</CelText>
+        <CelText type="H6" weight="500">{ formatter.crypto(fakeFormData.amountCollateralCrypto, fakeFormData.coin) }</CelText>
+        <CelText type="H6" weight="300">{ formatter.usd(fakeFormData.amountCollateralUSD) }</CelText>
 
-      <Separator margin="16 0 16 0"/>
-      <CelText type="H6" weight="300">Term of loan</CelText>
-      <CelText type="H6" weight="500">{ formData.termOfLoan } months</CelText>
+        <Separator margin="16 0 16 0"/>
+        <CelText type="H6" weight="300">Term of loan</CelText>
+        <CelText type="H6" weight="500">{ fakeFormData.termOfLoan } months</CelText>
 
-      <Separator margin="16 0 16 0"/>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <View style={{ maxWidth: '45%', paddingHorizontal: 10 }}>
-          <CelText type="H3" weight="600" align="center">{ formatter.percentage(formData.interest) }%</CelText>
-          <CelText type="H6" weight="300" align="center">Annual interest rate</CelText>
+        <Separator margin="16 0 16 0"/>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+          <View style={{ maxWidth: '45%', paddingHorizontal: 10 }}>
+            <CelText type="H3" weight="600" align="center">{ formatter.percentage(fakeFormData.ltv.interest) }%</CelText>
+            <CelText type="H6" weight="300" align="center">Annual interest rate</CelText>
+          </View>
+
+          <Separator vertical/>
+
+          <View style={{ width: '45%', paddingHorizontal: 5 }}>
+            <CelText type="H3" weight="600" align="center">{ formatter.usd(fakeFormData.monthlyPayment) }</CelText>
+            <CelText type="H6" weight="300" align="center">Monthly interest payment</CelText>
+          </View>
         </View>
 
-        <Separator vertical/>
+        <Separator margin="16 0 22 0"/>
+        <CelText type="H4" align="center">By applying for a loan you agree to our</CelText>
+        <CelText type="H4" align="center">Terms of Service</CelText>
 
-        <View style={{ width: '45%', paddingHorizontal: 5 }}>
-          <CelText type="H3" weight="600" align="center">{ formatter.usd(formData.monthlyPayment) }</CelText>
-          <CelText type="H6" weight="300" align="center">Monthly interest payment</CelText>
-        </View>
+        <CelButton onPress={() => onConfirm(formData)} margin="22 0 0 0">Initiate loan</CelButton>
       </View>
-
-      <Separator margin="16 0 22 0"/>
-      <CelText type="H4" align="center">By applying for a loan you agree to our</CelText>
-      <CelText type="H4" align="center">Terms of Service</CelText>
-
-      <CelButton onPress={onConfirm} margin="22 0 0 0">Initiate loan</CelButton>
-    </View>
-  </CelModal>
-)
+    </CelModal>
+  )
+}
 
 BorrowConfirmModal.propTypes = {
-  formData: PropTypes.instanceOf(Object).isRequired,
+  formData: PropTypes.instanceOf(Object),
   onConfirm: PropTypes.func.isRequired,
 }
 
