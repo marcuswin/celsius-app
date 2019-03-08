ce1826b11ab167a973ee85bdfaa0d5e4ecc0d7ef
Nikola Sabelnik <sabelnikola@gmail.com>
2019-03-08 14:02:10 +0100

    CN-2036 As a user ISBAT choose a loan option

--- app/components/screens/BorrowLoanOption/BorrowLoanOption.js
+++ app/components/screens/BorrowLoanOption/BorrowLoanOption.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { View } from 'react-native';
+import { View } from 'react-native';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
@@ -10,9 +10,19 @@ import * as appActions from "../../../redux/actions";
 import CelText from '../../atoms/CelText/CelText';
 import RegularLayout from '../../layouts/RegularLayout/RegularLayout';
 import CelButton from '../../atoms/CelButton/CelButton';
+import ProgressBar from "../../atoms/ProgressBar/ProgressBar";
+import Card from "../../atoms/Card/Card";
+import formatter from "../../../utils/formatter";
 
 @connect(
-  () => ({}),
+  state => ({
+    formLoan: {
+      loanAmount: 1000,
+    },
+    formData: state.forms.formData,
+    ltv: state.loans.ltvs,
+    currencyRates: state.currencies.currencyRatesShort,
+  }),
   dispatch => ({ actions: bindActionCreators(appActions, dispatch) }),
 )
 class BorrowLoanOption extends Component {
@@ -21,7 +31,7 @@ class BorrowLoanOption extends Component {
     // text: PropTypes.string
   };
   static defaultProps = {
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -29,19 +39,62 @@ class BorrowLoanOption extends Component {
       header: {
         title: "BorrowLoanOption Screen",
         left: "back",
-        right: "profile"
-      }
+        right: "info"
+      },
     };
   }
 
+  initializeFormDatas = (interest, monthlyPayment, amountCollateralUsd, amountCollateralCrypto, loanToValue) => {
+    const {actions} = this.props;
+
+    actions.updateFormFields({
+      interest,
+      monthlyPayment,
+      amountCollateralCrypto,
+      amountCollateralUsd,
+      ltv: loanToValue
+    })
+  };
+
+  renderInterestCard = (ltv) => {
+    const {formLoan, currencyRates, navigation} = this.props;
+    const coin = navigation.getParam('coin');
+
+    const crypto = currencyRates[coin.toLowerCase()];
+    const monthlyPayment = formLoan.loanAmount * ltv.interest / 12;
+    const amountCollateralUsd = formLoan.loanAmount / ltv.percent;
+    const amountCollateralCrypto = (amountCollateralUsd / crypto);
+    const interest = formatter.percentage(ltv.interest);
+    const loanToValue = ltv.percent;
+
+    // ToDo:(ns) add opacity on card if ineligible for LTV option
+
+      return (
+          <View>
+            <Card onPress={() => this.initializeFormDatas(interest, monthlyPayment, amountCollateralUsd, amountCollateralCrypto, loanToValue) }
+                  padding="15 15 15 15"
+                  margin="15 20 15 20"
+            >
+              <CelText weight={"300"} type={"H6"}>{`$${formatter.round(monthlyPayment)} per month`}</CelText>
+              <CelText weight={"600"} type={"H3"}>{`${interest}% interest rate`}</CelText>
+              <CelText weight={"300"} type={"H6"}>{`Locking ${formatter.crypto(amountCollateralCrypto)} ${coin} as collateral`}</CelText>
+            </Card>
+          </View>
+    )
+  };
+
   render() {
     const { header } = this.state;
-    const { actions } = this.props;
+    const { actions, ltv } = this.props;
     // const style = BorrowLoanOptionStyle();
-    
+
     return (
       <RegularLayout header={header}>
-        <CelText>Hello BorrowLoanOption</CelText>
+        <ProgressBar steps={6} currentStep={3}/>
+
+        <CelText weight={"300"} margin={"20 0 20 0"}>Choose your loan option:</CelText>
+
+        {ltv.map(ltvs => this.renderInterestCard(ltvs))}
 
         <CelButton onPress={() => actions.navigateTo('BorrowLoanTerm')}>
           Choose loan term
