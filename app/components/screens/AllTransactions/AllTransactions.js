130aeeebfee43165115a1d4a50e7a7ea13ded6af
StefanBurscher <stefan.burscher@gmail.com>
2019-03-08 14:14:47 +0100

    CN-2118: new TransactionHistory implemented on all transactions screen

--- app/components/screens/AllTransactions/AllTransactions.js
+++ app/components/screens/AllTransactions/AllTransactions.js
@@ -1,26 +1,11 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { connect } from 'react-redux';
-import { bindActionCreators } from "redux";
 
 import testUtil from "../../../utils/test-util";
-import * as appActions from "../../../redux/actions";
 import AllTransactionsStyle from "./AllTransactions.styles";
 import RegularLayout from '../../layouts/RegularLayout/RegularLayout';
 import TransactionsHistory from '../../molecules/TransactionsHistory/TransactionsHistory';
-import transactionsUtil from "../../../utils/transactions-util";
-import API from '../../../constants/API';
-import apiUtil from '../../../utils/api-util';
-import LoadingState from '../../atoms/LoadingState/LoadingState';
 
-@connect(
-  state => ({
-    transactions: state.transactions.transactionList,
-    currencyRatesShort: state.currencies.currencyRatesShort,
-    callsInProgress: state.api.callsInProgress
-  }),
-  dispatch => ({ actions: bindActionCreators(appActions, dispatch) }),
-)
 class AllTransactions extends Component {
   static propTypes = {};
   static defaultProps = {}
@@ -36,29 +21,14 @@ class AllTransactions extends Component {
     };
   }
 
-  componentDidMount() {
-    const { actions } = this.props
-    actions.getAllTransactions();
-  }
-
   render() {
-    const { actions, currencyRatesShort, transactions, callsInProgress } = this.props
     const { header } = this.state;
-    const transactionsArray = transactionsUtil.filterTransactions(transactions);
-    const loadingTransactions = apiUtil.areCallsInProgress([API.GET_ALL_TRANSACTIONS], callsInProgress);
     const style = AllTransactionsStyle();
 
     return (
       <RegularLayout header={header}>
         <View style={style.container}>
-          {loadingTransactions ?
-            <LoadingState />
-            : <TransactionsHistory
-              transactions={transactionsArray}
-              currencyRatesShort={currencyRatesShort}
-              navigateTo={actions.navigateTo}
-            />
-          }
+          <TransactionsHistory />
         </View>
       </RegularLayout>
     )
