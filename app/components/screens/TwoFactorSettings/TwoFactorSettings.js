556542cbc5389911db6d522e7f3367869625eec8
pantoandjelic <panto.andjelic@gmail.com>
2019-03-19 13:01:47 +0100

    CN-2087 As a user ISBAT set my 2FA (applied design)

--- app/components/screens/TwoFactorSettings/TwoFactorSettings.js
+++ app/components/screens/TwoFactorSettings/TwoFactorSettings.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import { Clipboard, View, Linking, TouchableOpacity } from 'react-native';
+import { View, Linking } from 'react-native';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
 import QRCode from 'react-qr-code';
-import STYLES from "../../../constants/STYLES";
 
 import testUtil from "../../../utils/test-util";
 import * as appActions from "../../../redux/actions";
 import CelText from '../../atoms/CelText/CelText';
 import RegularLayout from '../../layouts/RegularLayout/RegularLayout';
-// import TwoFactorSettingsStyle from './TwoFactorSettings.styles'
-import Icon from '../../atoms/Icon/Icon'
+import TwoFactorSettingsStyle from './TwoFactorSettings.styles'
 import CelButton from '../../atoms/CelButton/CelButton'
 import LoadingScreen from '../LoadingScreen/LoadingScreen'
+import Card from '../../atoms/Card/Card'
+import CopyButton from '../../atoms/CopyButton/CopyButton'
+import Separator from '../../atoms/Separator/Separator'
 
 @connect(
   state => ({
@@ -58,19 +59,10 @@ class TwoFactorSettings extends Component {
 
   getQRCode = secret => `otpauth://totp/Celsius?secret=${secret}&issuer=Celsius`;
 
-  copyTwoFactorSecret = () => {
-    const { actions } = this.props;
-    const { secret } = this.state;
-
-    actions.showMessage("success", "Secret copied to clipboard!");
-
-    Clipboard.setString(secret);
-  };
-
   render() {
-    const { actions } = this.props;
-    const { secret, secretLoaded } = this.state;
-    // const style = TwoFactorSettingsStyle();
+    const { actions } = this.props
+    const { secret, secretLoaded } = this.state
+    const style = TwoFactorSettingsStyle()
 
     if (!secretLoaded) return <LoadingScreen/>
 
@@ -79,35 +71,26 @@ class TwoFactorSettings extends Component {
         <CelText align='center' type='H4'>
           Scan the QR code or enter the code manually in your auth app.
         </CelText>
-          <View style={{marginTop: 20, borderColor: 'rgba(61,72,83,0.1)', borderWidth: 5}}>
-            <QRCode
-              value={this.getQRCode(secret)}
-              size={141}
-              bgColor='black'
-              fgColor='white'
-            />
-          </View>
+        <Card styles={{alignItems: 'center', marginTop: 25}} >
+          <QRCode
+            value={this.getQRCode(secret)}
+            size={141}
+            bgColor='black'
+            fgColor='white'
+          />
+          <CelText align='center' style={style.secretText} onPress={() => Linking.openURL(this.getQRCode(secret))}>{secret}</CelText>
 
-          <View style={{marginTop: 20, borderWidth: 5, borderColor: 'rgba(61,72,83,0.1)', maxWidth: '80%'}}>
-            <View style={{backgroundColor: 'rgba(61,72,83,0.1)', padding: 10}}>
-              <CelText align='center' style={{color: STYLES.COLORS.CELSIUS, textDecorationLine: 'underline',}} onPress={() => Linking.openURL(this.getQRCode(secret))}>{secret}</CelText>
-            </View>
-            <View style={{padding: 10}}>
-              <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}} onPress={this.copyTwoFactorSecret}>
-                <Icon
-                  name={'CopyIcon'}
-                  width='20' height='20'
-                  fill='rgba(61,72,83,0.3)'
-                />
-                <CelText type='H4' margin='0 0 0 10'>Copy</CelText>
-              </TouchableOpacity>
-            </View>
+          <View style={{paddingVertical: 15, width: '100%'}}>
+            <Separator size={2} opacity={0.07} s/>
           </View>
-          <View style={{flex: 1, justifyContent: 'flex-end'}}>
-            <CelButton onPress={() => {actions.navigateTo('TwoFaAuthAppConfirmationCode')}}>
-              Continue
-            </CelButton>
-          </View>
+          <CopyButton copyText={secret} onCopy={() => actions.showMessage("success", "Secret copied to clipboard!")}/>
+        </Card>
+
+        <View style={{flex: 1, justifyContent: 'flex-end'}}>
+          <CelButton onPress={() => {actions.navigateTo('TwoFaAuthAppConfirmationCode')}}>
+            Continue
+          </CelButton>
+        </View>
       </RegularLayout>
     );
   }
