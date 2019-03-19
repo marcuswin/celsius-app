89a8309981164a5dc06fb812a0d6bc0f423eab35
Filip Jovakaric <filip.jovakaric@gmail.com>
2019-03-19 17:07:30 +0100

    CN-2102: upgrading initial register screen and logic

--- app/components/screens/RegisterInitial/RegisterInitial.js
+++ app/components/screens/RegisterInitial/RegisterInitial.js
@@ -8,9 +8,9 @@ import * as appActions from "../../../redux/actions";
 import CelText from '../../atoms/CelText/CelText';
 import CelInput from '../../atoms/CelInput/CelInput';
 import CelButton from '../../atoms/CelButton/CelButton';
-import Separator from '../../atoms/Separator/Separator';
 import ProgressBar from '../../atoms/ProgressBar/ProgressBar';
 import AuthLayout from '../../layouts/AuthLayout/AuthLayout';
+import SocialLogin from "../../organisms/SocialLogin/SocialLogin";
 
 @connect(
   state => ({
@@ -30,15 +30,24 @@ class Register extends Component {
   render() {
     const { formData, actions } = this.props;
 
+    const isUsingSocial = formData.googleId || formData.facebookId || formData.twitterId;
     return (
       <AuthLayout>
         <CelText margin="0 0 30 0" align="center" type="H1">Join Celsius</CelText>
-        <CelInput type="text" field="firstname" placeholder="First name" />
-        <CelInput type="text" field="lastname" placeholder="Last name" />
-        <CelInput type="text" field="email" placeholder="E-mail" />
-        <CelInput type="password" field="password" placeholder="Password" value={formData.password} />
-        <CelButton margin="10 0 40 0" onPress={() => { actions.navigateTo('RegisterEnterPhone') }} iconRight="IconArrowRight">Create account</CelButton>
-        <Separator text="or Register with social media" />
+
+        <SocialLogin type="register" actions={actions} />
+
+        <CelInput margin="20 0 20 0" type="text" field="firstName" value={formData.firstName} placeholder="First name" />
+        <CelInput type="text" field="lastName" value={formData.lastName} placeholder="Last name" />
+
+
+        <CelInput editable={!isUsingSocial} type="text" value={formData.email} field="email" placeholder="E-mail" />
+
+        { !isUsingSocial && <CelInput type="password" field="password" placeholder="Password" value={formData.password} /> }
+
+        <CelButton margin="10 0 40 0" onPress={() => { actions.navigateTo('RegisterSetPin') }} iconRight="IconArrowRight">Create account</CelButton>
+
+        <CelText color="rgba(61,72,83,0.5)" type="H4" margin="30 20 0 20" align="center">By creating an account you agree to our Terms of Use</CelText>
       </AuthLayout>
     );
   }
