10de966157d7cf51987f49185b5df64c6cbf28ab
pantoandjelic <panto.andjelic@gmail.com>
2019-03-19 13:56:09 +0100

    CN-2190 On IOS, pressing outside of the button still triggers button press event

--- app/components/atoms/Icon/Icon.js
+++ app/components/atoms/Icon/Icon.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SvgIcon from 'react-native-svg-icon';
 import PropTypes from 'prop-types';
+import { View } from 'react-native'
 
 import testUtil from "../../../utils/test-util";
 import Svgs from '../../../constants/SVGS';
@@ -18,7 +19,7 @@ class Icon extends Component {
     const { name, fill, style } = this.props
 
     const viewBox = Svgs[`${name}ViewBox`] || this.props.viewBox;
-    return <SvgIcon viewBox={viewBox} name={name} svgs={Svgs} fill={fill} {...this.props} style={[{ alignSelf: 'center', justifyContent: 'center' }, style]} />;
+    return <View style={{overflow: 'hidden'}}><SvgIcon viewBox={viewBox} name={name} svgs={Svgs} fill={fill} {...this.props} style={[{ alignSelf: 'center', justifyContent: 'center' }, style]} /></View>;
   }
 }
 
