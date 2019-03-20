5049b8e87e1b766ba6b00fb1dd4d95cdff7fa9b4
Filip Jovakaric <filip.jovakaric@gmail.com>
2019-03-20 23:38:18 +0100

    CN-2094: removing phone screens from authFlow

--- app/navigator/flows/authFlow.js
+++ app/navigator/flows/authFlow.js
@@ -4,20 +4,14 @@ import { defaultNavigationOptions, transitionConfig } from '../navigationConfig'
 import Welcome from '../../components/screens/Welcome/Welcome'
 import Login from '../../components/screens/Login/Login'
 import RegisterInitial from '../../components/screens/RegisterInitial/RegisterInitial'
-import RegisterEnterPhone from '../../components/screens/RegisterEnterPhone/RegisterEnterPhone'
-import RegisterVerifyPhone from '../../components/screens/RegisterVerifyPhone/RegisterVerifyPhone'
 import RegisterSetPin from '../../components/screens/RegisterSetPin/RegisterSetPin'
-import SelectCountry from '../../components/screens/SelectCountry/SelectCountry'
 
 const authFlow = {
   screens: {
     Welcome,
     Login,
     RegisterInitial,
-    RegisterEnterPhone,
-    RegisterVerifyPhone,
     RegisterSetPin,
-    SelectCountry,
   },
   props: {
     initialRouteName: 'Welcome',
