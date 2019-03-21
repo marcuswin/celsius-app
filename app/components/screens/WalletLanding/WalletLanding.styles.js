4d47a4a88ae44c44cd3a4e6a81aedcf490e7c521
pantoandjelic <panto.andjelic@gmail.com>
2019-03-21 11:59:59 +0100

    Re-indented wallet landing styles

--- app/components/screens/WalletLanding/WalletLanding.styles.js
+++ app/components/screens/WalletLanding/WalletLanding.styles.js
@@ -2,59 +2,61 @@ import { getThemedStyle } from '../../../utils/styles-util';
 
 
 const base = {
-    coinCardContainer: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between'
-    },
-    addMoreCoinsGrid: {
-        borderWidth: 1,
-        borderStyle: 'dashed',
-        borderRadius: 10,
-        width: '46%',
-        height: '18%',
-        borderColor: 'gray',
-        marginTop: 5
-    },
+  coinCardContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  addMoreCoinsGrid: {
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10,
+    width: '46%',
+    height: '18%',
+    borderColor: 'gray',
+    marginTop: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 
-    addMoreCoinsList: {
-        borderWidth: 1,
-        borderStyle: 'dashed',
-        borderRadius: 10,
-        width: '100%',
-        height: '10%',
-        borderColor: 'gray',
-        marginTop: 5,
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
+  addMoreCoinsList: {
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10,
+    width: '100%',
+    height: '10%',
+    borderColor: 'gray',
+    marginTop: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 
-    coinsTextGrid: {
-        marginTop: '35%',
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'space-between'
+  coinsTextGrid: {
+    marginTop: '35%',
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'space-between'
 
-    },
+  },
 
-    coinsTextList: {
-        marginTop: '15%',
-        alignItems: 'center',
-        alignContent: 'center',
-        // justifyContent: 'space-between'
-    }
+  coinsTextList: {
+    marginTop: '15%',
+    alignItems: 'center',
+    alignContent: 'center',
+    // justifyContent: 'space-between'
+  }
 }
 
 const themed = {
-    light: {
-    },
+  light: {
+  },
 
-    dark: {
-    },
+  dark: {
+  },
 
-    celsius: {
-    }
+  celsius: {
+  }
 }
 
 const WalletLandingStyle = () => getThemedStyle(base, themed);
