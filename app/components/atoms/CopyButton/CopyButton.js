a9dc53800ce8c226a70d586bb877c5bf13689696
nemanja <nemanjakrstonic2@gmail.com>
2019-04-22 11:47:39 +0200

    CN-2507: Copy/Share button clickable

--- app/components/atoms/CopyButton/CopyButton.js
+++ app/components/atoms/CopyButton/CopyButton.js
@@ -5,26 +5,28 @@ import { Clipboard, TouchableOpacity } from 'react-native';
 import testUtil from "../../../utils/test-util";
 
 import CelText from '../CelText/CelText';
+import CopyButtonStyle from './CopyButton.styles';
 
 const CopyButton = (props) => {
   // To do add propTypes
-  const { copyText, onCopy, text='Copy',color } = props;
-  
+  const { copyText, onCopy, text = 'Copy', color } = props;
+  const style = CopyButtonStyle();
+
 
   return (
-    <TouchableOpacity onPress={() => {Clipboard.setString(copyText); onCopy()}}>
-        <CelText color={color}>{text}</CelText>
+    <TouchableOpacity style={ style.container} onPress={() => { Clipboard.setString(copyText); onCopy() }}>
+      <CelText color={color}>{text}</CelText>
     </TouchableOpacity>
   )
-}
+} 
 
 CopyButton.propTypes = {
   copyText: PropTypes.string.isRequired,
   onCopy: PropTypes.func
 };
 
-CopyButton.defaultProps ={
-  onCopy: () => {}
+CopyButton.defaultProps = {
+  onCopy: () => { }
 };
 
 export default testUtil.hookComponent(CopyButton);
