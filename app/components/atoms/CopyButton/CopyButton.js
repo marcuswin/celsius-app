5ef7d474b8c7a1ee609942b3e7677592e38e56fe
nemanja <nemanjakrstonic2@gmail.com>
2019-04-23 11:44:37 +0200

    Pull request V3-bugfixes #30 fixes

--- app/components/atoms/CopyButton/CopyButton.js
+++ app/components/atoms/CopyButton/CopyButton.js
@@ -8,25 +8,30 @@ import CelText from '../CelText/CelText';
 import CopyButtonStyle from './CopyButton.styles';
 
 const CopyButton = (props) => {
+
+
   // To do add propTypes
-  const { copyText, onCopy, text = 'Copy', color } = props;
+  const { copyText, onCopy, text, color } = props;
   const style = CopyButtonStyle();
 
 
   return (
-    <TouchableOpacity style={ style.container} onPress={() => { Clipboard.setString(copyText); onCopy() }}>
+    <TouchableOpacity style={style.container} onPress={() => { Clipboard.setString(copyText); onCopy() }}>
       <CelText color={color}>{text}</CelText>
     </TouchableOpacity>
   )
-} 
+}
 
 CopyButton.propTypes = {
   copyText: PropTypes.string.isRequired,
-  onCopy: PropTypes.func
+  onCopy: PropTypes.func,
+  text: PropTypes.string,
+  color: PropTypes.string
 };
 
 CopyButton.defaultProps = {
-  onCopy: () => { }
+  onCopy: () => { },
+  text: 'Copy',
 };
 
 export default testUtil.hookComponent(CopyButton);
