d1206e1cf37374566d69c6740ea063361259b416
Nikola Sabelnik <sabelnikola@gmail.com>
2019-04-26 10:45:07 +0200

    CN-2626 Hide Earn in CEL button from all places

--- app/components/molecules/CelInterestCard/CelInterestCard.js
+++ app/components/molecules/CelInterestCard/CelInterestCard.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Platform, Switch } from "react-native";
+// import { Platform } from "react-native";
 
 import testUtil from "../../../utils/test-util";
 import Card from "../../atoms/Card/Card";
 import CelText from "../../atoms/CelText/CelText";
-import STYLES from "../../../constants/STYLES";
-import Spinner from "../../atoms/Spinner/Spinner";
+// import STYLES from "../../../constants/STYLES";
+// import Spinner from "../../atoms/Spinner/Spinner";
 
 class CelInterestCard extends Component {
   static propTypes = {
@@ -31,10 +31,10 @@ class CelInterestCard extends Component {
   }
 
   render() {
-    const { interestInCel, interestBonus, tier } = this.props
-    const { loading } = this.state
+    const { interestBonus, tier } = this.props
+    // const { loading } = this.state
     if (tier === 'NONE') return null
-    const falseColor = Platform.OS === 'ios' ? "transparent" : STYLES.COLORS.DARK_GRAY3;
+    // const falseColor = Platform.OS === 'ios' ? "transparent" : STYLES.COLORS.DARK_GRAY3;
 
     return (
       <Card close margin={"30 0 0 0"}>
@@ -42,19 +42,6 @@ class CelInterestCard extends Component {
         <CelText color='gray' type={"H4"} weight={"300"} margin={"15 0 15 0"}>
           Switch to earning interest in CEL, and earn { interestBonus }% better rates.
         </CelText>
-
-        { loading ? (
-          <Spinner size={35} />
-        ) : (
-          <Switch
-            thumbColor={STYLES.COLORS.WHITE}
-            ios_backgroundColor={STYLES.COLORS.DARK_GRAY3}
-            trackColor={{ false: falseColor, true: STYLES.COLORS.GREEN }}
-            value={interestInCel}
-            onValueChange={this.handleValueChange}
-          />
-        )}
-
       </Card>
     );
   }
--- app/components/screens/WalletSettings/WalletSettings.js
+++ app/components/screens/WalletSettings/WalletSettings.js
@@ -1,56 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import PropTypes from 'prop-types';
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { Platform, Switch } from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Platform, Switch, View } from "react-native";
 
-import testUtil from '../../../utils/test-util'
-import * as appActions from '../../../redux/actions'
+import testUtil from "../../../utils/test-util";
+import * as appActions from "../../../redux/actions";
 // import WalletSettingsStyle from "./WalletSettings.styles";
-import RegularLayout from '../../layouts/RegularLayout/RegularLayout'
-import Separator from '../../atoms/Separator/Separator'
-import IconButton from '../../organisms/IconButton/IconButton'
-import STYLES from '../../../constants/STYLES'
+import RegularLayout from "../../layouts/RegularLayout/RegularLayout";
+// import Separator from '../../atoms/Separator/Separator'
+// import IconButton from '../../organisms/IconButton/IconButton'
+import STYLES from "../../../constants/STYLES";
 import apiUtil from "../../../utils/api-util";
 import API from "../../../constants/API";
 import LoadingScreen from "../LoadingScreen/LoadingScreen";
 import Spinner from "../../atoms/Spinner/Spinner";
+import Card from "../../atoms/Card/Card";
+import CelText from "../../atoms/CelText/CelText";
 
 @connect(
   state => ({
     appSettings: state.user.appSettings,
     formData: state.forms.formData,
-    callsInProgress: state.api.callsInProgress,
+    callsInProgress: state.api.callsInProgress
   }),
   dispatch => ({ actions: bindActionCreators(appActions, dispatch) })
 )
 class WalletSettings extends Component {
   static propTypes = {
     // text: PropTypes.string
-  }
-  static defaultProps = {}
+  };
+  static defaultProps = {};
 
   static navigationOptions = () => ({
-    title: 'Wallet'
-  })
+    title: "Wallet"
+  });
 
-  componentDidMount () {
-    const { actions } = this.props
-    actions.getUserAppSettings()
+  componentDidMount() {
+    const { actions } = this.props;
+    actions.getUserAppSettings();
   }
 
   changeInterestEarn = () => {
-    const { actions, appSettings } = this.props
-    actions.setUserAppSettings({ interest_in_cel: !appSettings.interest_in_cel })
-  }
+    const { actions, appSettings } = this.props;
+    actions.setUserAppSettings({ interest_in_cel: !appSettings.interest_in_cel });
+  };
 
   rightSwitch = () => {
-    const { appSettings, callsInProgress } = this.props
-    const isIos = Platform.OS === 'ios'
+    const { appSettings, callsInProgress } = this.props;
+    const isIos = Platform.OS === "ios";
     const falseColor = isIos ? "transparent" : STYLES.COLORS.DARK_GRAY3;
     const loading = apiUtil.areCallsInProgress([API.SET_APP_SETTINGS], callsInProgress);
     return loading ? (
-      <Spinner size={30} />
+      <Spinner size={30}/>
     ) : (
       <Switch
         value={appSettings.interest_in_cel}
@@ -59,32 +61,37 @@ class WalletSettings extends Component {
         ios_backgroundColor={STYLES.COLORS.DARK_GRAY3}
         trackColor={{ false: falseColor, true: STYLES.COLORS.GREEN }}
       />
-    )
-  }
+    );
+  };
 
-  render () {
-    const { callsInProgress } = this.props
+  render() {
+    const { callsInProgress } = this.props;
 
     const loading = apiUtil.areCallsInProgress([API.GET_APP_SETTINGS], callsInProgress);
-    if (loading) return <LoadingScreen />
+    if (loading) return <LoadingScreen/>;
 
     return (
       <RegularLayout>
         {/* <IconButton right={<CelText>USD</CelText>}>Default currency</IconButton>
         <IconButton margin='0 0 20 0'>Default view</IconButton> */}
-        <Separator text='INTEREST' />
+        {/* <Separator text='INTEREST' />*/}
 
-        <IconButton
-          margin={"20 0 20 0"}
-          right={this.rightSwitch()}
-          hideIconRight
-          onPress={() => this.changeInterestEarn()}
-        >
-          Earn interest in CEL
-        </IconButton>
+        {/* <IconButton */}
+        {/* margin={"20 0 20 0"} */}
+        {/* right={this.rightSwitch()} */}
+        {/* hideIconRight */}
+        {/* onPress={() => this.changeInterestEarn()} */}
+        {/* > */}
+        {/* Earn interest in CEL */}
+        {/* </IconButton> */}
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <Card size={"half"}>
+            <CelText align={"center"} weight={"500"} type={"H5"}>COMING SOON!</CelText>
+          </Card>
+        </View>
       </RegularLayout>
-    )
+    );
   }
 }
 
-export default testUtil.hookComponent(WalletSettings)
+export default testUtil.hookComponent(WalletSettings);
