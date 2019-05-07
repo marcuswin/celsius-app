f99f2f0e5c08b25529e0cdb941c7018761f724eb
StefanBurscher <stefan.burscher@gmail.com>
2019-05-07 16:50:13 +0200

    CN-2703: Welcome screen responsive changes

--- app/components/atoms/Loader/Loader.js
+++ app/components/atoms/Loader/Loader.js
@@ -1,70 +1,80 @@
-import React, {Component} from 'react';
-import { View, Animated } from "react-native";
-import STYLES from "../../../constants/STYLES";
-import testUtil from "../../../utils/test-util";
-
-
+import React, { Component } from 'react'
+import { View, Animated } from 'react-native'
+import STYLES from '../../../constants/STYLES'
+import testUtil from '../../../utils/test-util'
 
 class ProgressBar extends Component {
   static defaultProps = {
     height: 10,
-    width: "50%",
+    width: '50%',
     borderWidth: 2,
     borderRadius: 4,
-    borderColor: "white",
+    borderColor: 'white',
     barColor: STYLES.COLORS.MEDIUM_GRAY3
-  };
+  }
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
-      progress: 0,
-    };
+      progress: 0
+    }
   }
   // lifecycle methods
-  componentWillMount() {
-    this.animation = new Animated.Value
-    (this.props.progress);
+  componentWillMount () {
+    this.animation = new Animated.Value(this.props.progress)
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate (prevProps) {
     if (prevProps.progress !== this.props.progress) {
       Animated.timing(this.animation, {
-        toValue: this.props.progress,
+        toValue: this.props.progress
       }).start()
     }
   }
 
   // event hanlders
   // rendering methods
-  render() {
-    const {height, borderColor, borderWidth, borderRadius, barColor, width} = this.props;
+  render () {
+    const {
+      height,
+      borderColor,
+      borderWidth,
+      borderRadius,
+      barColor,
+      width
+    } = this.props
 
     const widthInterpolated = this.animation.interpolate({
-      inputRange: [0,1],
-      outputRange: ["0%", "100%"],
-      extrapolate: "clamp"
-    });
+      inputRange: [0, 1],
+      outputRange: ['0%', '100%'],
+      extrapolate: 'clamp'
+    })
 
     return (
-      <View style={{width, height}}>
-        <View style={{flex: 1, borderColor, borderWidth, borderRadius, backgroundColor: "white"}}>
+        <View
+          style={{
+            width, height,
+            borderColor,
+            borderWidth,
+            borderRadius,
+            backgroundColor: STYLES.COLORS.LIGHT_GRAY
+          }}
+        >
           <Animated.View
             style={{
-              position: "absolute",
+              position: 'absolute',
               left: 0,
               top: 0,
               bottom: 0,
               width: widthInterpolated,
               backgroundColor: barColor,
-              borderRadius: 4,
+              borderRadius: 4
             }}
           />
         </View>
-      </View>
-    );
+    )
   }
 }
 
-export default testUtil.hookComponent(ProgressBar);
+export default testUtil.hookComponent(ProgressBar)
--- app/components/screens/Home/Home.js
+++ app/components/screens/Home/Home.js
@@ -1,14 +1,18 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { View, Image } from "react-native";
-import * as appActions from "../../../redux/actions";
-import { KYC_STATUSES, RANDOM_MESSAGES } from "../../../constants/DATA";
-import Loader from "../../atoms/Loader/Loader";
-import { heightPercentageToDP, widthPercentageToDP } from "../../../utils/styles-util";
-import CelText from "../../atoms/CelText/CelText";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { View, Image, ScrollView, SafeAreaView } from 'react-native'
+import * as appActions from '../../../redux/actions'
+import { KYC_STATUSES, RANDOM_MESSAGES } from '../../../constants/DATA'
+import Loader from '../../atoms/Loader/Loader'
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+  getPadding
+} from '../../../utils/styles-util'
+import CelText from '../../atoms/CelText/CelText'
 
-const apiCalls = [];
+const apiCalls = []
 
 @connect(
   state => ({
@@ -19,20 +23,19 @@ const apiCalls = [];
   dispatch => ({ actions: bindActionCreators(appActions, dispatch) })
 )
 class Home extends Component {
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const { callsInProgress } = nextProps;
+  static getDerivedStateFromProps (nextProps, prevState) {
+    const { callsInProgress } = nextProps
 
     if (callsInProgress[0] && apiCalls.indexOf(callsInProgress[0]) === -1) {
-      apiCalls.push(callsInProgress[0]);
-      return { progress: prevState.progress + (1 / 6) };
+      apiCalls.push(callsInProgress[0])
+      return { progress: prevState.progress + 1 / 6 }
       // six is current number of calls being called while loading app
     }
-    return null;
+    return null
   }
 
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
       progress: 0,
@@ -40,89 +43,123 @@ class Home extends Component {
     }
   }
 
-  async componentDidMount() {
-    const { actions, appInitialized } = this.props;
-    if (!appInitialized) await actions.initCelsiusApp();
+  async componentDidMount () {
+    const { actions, appInitialized } = this.props
+    if (!appInitialized) await actions.initCelsiusApp()
   }
 
-  componentDidUpdate(prevProps) {
-    const { user } = this.props;
-    if (prevProps.appInitialized === false && this.props.appInitialized === true) {
+  componentDidUpdate (prevProps) {
+    const { user } = this.props
+    if (
+      prevProps.appInitialized === false &&
+      this.props.appInitialized === true
+    ) {
       if (user.id) {
-        if (user.kyc && user.kyc.status === KYC_STATUSES.passed && user.has_pin) {
-          return prevProps.actions.navigateTo("VerifyProfile", {activeScreen: "WalletLanding"});
+        if (
+          user.kyc &&
+          user.kyc.status === KYC_STATUSES.passed &&
+          user.has_pin
+        ) {
+          return prevProps.actions.navigateTo('VerifyProfile', {
+            activeScreen: 'WalletLanding'
+          })
         }
         if (!user.has_pin) {
-          return prevProps.actions.navigateTo("RegisterSetPin");
+          return prevProps.actions.navigateTo('RegisterSetPin')
         }
-        return prevProps.actions.navigateTo("WalletFab");
+        return prevProps.actions.navigateTo('WalletFab')
       }
-      return prevProps.actions.navigateTo("Welcome");
+      return prevProps.actions.navigateTo('Welcome')
     }
   }
 
-  render() {
-    const {randomMsg} = this.state;
+  render () {
+    const { randomMsg } = this.state
+    const paddings = getPadding('0 20 0 20')
+
     return (
-      <View style={{ flex: 1 }}>
-        <View
-          style={{ justifyContent: "center", alignItems: "center", padding: 40, marginTop: heightPercentageToDP("15%") }}>
-          <Image source={require("../../../../assets/images/splashScreen-celsius-new.png")}
-                style={{
-                  resizeMode: "contain",
-                  width: widthPercentageToDP("33%"),
-                  height: widthPercentageToDP("33%")
-                }}/>
-          <CelText align={"center"} margin={"20 0 10 0"} weight={"600"} type={"H2"}>{randomMsg.title}</CelText>
-          <CelText align={"center"} margin={"0 0 20 0"} type={"H4"} weight={"300"}>{randomMsg.text}</CelText>
-          <Loader progress={this.state.progress}/>
-        </View>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <Image
-              source={require("../../../../assets/images/PartnerLogos/BitGo.png")}
+      <ScrollView contentContainerStyle={[{ flexGrow: 1 }, paddings]}>
+        <SafeAreaView style={{ flex: 1, justifyContent: 'space-between' }}>
+          <View
+            style={{
+              marginTop: heightPercentageToDP('15%'),
+              alignItems: 'center'
+            }}
+          >
+            <Image
+              source={require('../../../../assets/images/splashScreen-celsius-new.png')}
               style={{
-                resizeMode: "contain",
-                width: widthPercentageToDP("18%"),
+                resizeMode: 'contain',
+                width: widthPercentageToDP('33%'),
+                height: widthPercentageToDP('33%'),
+                marginBottom: 20
+              }}
+            />
+            <CelText
+              align={'center'}
+              margin={'20 0 10 0'}
+              weight={'600'}
+              type={'H2'}
+            >
+              {randomMsg.title}
+            </CelText>
+            <CelText
+              align={'center'}
+              margin={'0 0 20 0'}
+              type={'H4'}
+              weight={'300'}
+            >
+              {randomMsg.text}
+            </CelText>
+            <Loader progress={this.state.progress} />
+          </View>
+          <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+            <Image
+              source={require('../../../../assets/images/PartnerLogos/BitGo.png')}
+              style={{
+                resizeMode: 'contain',
+                width: widthPercentageToDP('18%'),
                 marginLeft: 35,
                 marginRight: 5,
                 alignSelf: 'flex-end'
               }}
             />
             <Image
-              source={require("../../../../assets/images/PartnerLogos/DP.png")}
+              source={require('../../../../assets/images/PartnerLogos/DP.png')}
               style={{
-                resizeMode: "contain",
-                width: widthPercentageToDP("18%"),
+                resizeMode: 'contain',
+                width: widthPercentageToDP('18%'),
                 marginLeft: 5,
                 marginRight: 5,
                 alignSelf: 'flex-end'
               }}
             />
             <Image
-              source={require("../../../../assets/images/PartnerLogos/EY.png")}
+              source={require('../../../../assets/images/PartnerLogos/EY.png')}
               style={{
-                resizeMode: "contain",
-                width: widthPercentageToDP("18%"),
+                resizeMode: 'contain',
+                width: widthPercentageToDP('18%'),
                 marginLeft: 5,
                 marginRight: 5,
                 alignSelf: 'flex-end'
               }}
             />
             <Image
-              source={require("../../../../assets/images/PartnerLogos/mvp_workshop.png")}
+              source={require('../../../../assets/images/PartnerLogos/mvp_workshop.png')}
               style={{
-                resizeMode: "contain",
-                width: widthPercentageToDP("22%"),
+                resizeMode: 'contain',
+                width: widthPercentageToDP('22%'),
                 opacity: 0.7,
                 marginLeft: 0,
                 marginRight: 35,
                 alignSelf: 'flex-end'
               }}
             />
-        </View>
-      </View>
-    );
+          </View>
+        </SafeAreaView>
+      </ScrollView>
+    )
   }
 }
 
-export default Home;
+export default Home
