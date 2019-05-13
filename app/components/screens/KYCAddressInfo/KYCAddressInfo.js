3bfbe5d4a2418817f3221e59d5d5bdfc3b1bf66f
nemanja <nemanjakrstonic2@gmail.com>
2019-05-13 13:29:21 +0200

    CN-2449: Improved KYC input field

--- app/components/screens/KYCAddressInfo/KYCAddressInfo.js
+++ app/components/screens/KYCAddressInfo/KYCAddressInfo.js
@@ -28,7 +28,7 @@ class KYCAddressInfo extends Component {
   static defaultProps = {}
 
   static navigationOptions = () => ({
-    customCenterComponent: <ProgressBar steps={4} currentStep={2}/>,
+    customCenterComponent: <ProgressBar steps={4} currentStep={2} />,
     headerSameColor: true
   });
 
@@ -45,9 +45,18 @@ class KYCAddressInfo extends Component {
     const { actions } = this.props
 
     await actions.getProfileInfo()
-    this.setState({isLoading: false})
+    this.setState({ isLoading: false })
 
     this.initForm(this.props.user)
+  }
+
+  componentDidUpdate() {
+    const errors = Object.keys(this.props.formErrors)
+    if (errors.length > 0) {
+      if (errors.includes('street')) this.street.focus();
+      else if(errors.includes('city')) this.city.focus();
+      else if(errors.includes('zip')) this.zip.focus();
+    }
   }
 
   initForm = (user) => {
@@ -59,7 +68,7 @@ class KYCAddressInfo extends Component {
         flatNumber: user.flat_number,
         city: user.city,
         zip: user.zip,
-        country: {name: user.country},
+        country: { name: user.country },
         state: user.state
       })
     }
@@ -77,14 +86,14 @@ class KYCAddressInfo extends Component {
       state: formData.state,
     }
 
-    this.setState({updatingAddressInfoInProgress: true})
+    this.setState({ updatingAddressInfoInProgress: true })
     const response = await actions.updateProfileAddressInfo(updatedAddressInfo)
 
     if (response.success) {
       actions.navigateTo('KYCTaxpayer')
     }
 
-    this.setState({updatingAddressInfoInProgress: false})
+    this.setState({ updatingAddressInfoInProgress: false })
   }
 
   render() {
@@ -95,17 +104,17 @@ class KYCAddressInfo extends Component {
       <RegularLayout>
         <CelText type='H1' weight='bold' margin={'0 0 30 0'} align='center'>Address info</CelText>
 
-        <CelInput type="text" field="street" placeholder='Address' value={formData.street} error={formErrors.street} returnKeyType={"next"} blurOnSubmiting={false} onSubmitEditing={() => {this.flat.focus()}}/>
-        <CelInput type="text" field="flatNumber" placeholder='Apartment number (optional)' value={formData.flatNumber} refs={(input) => {this.flat = input}} returnKeyType={"next"} blurOnSubmiting={false} onSubmitEditing={() => {this.city.focus()}}/>
-        <CelInput type="text" field="city" placeholder='City' value={formData.city} error={formErrors.city} returnKeyType={"next"} refs={(input) => {this.city = input}} blurOnSubmiting={false} onSubmitEditing={() => {this.zip.focus()}}/>
-        <CelInput type="text" field="zip" placeholder='ZIP/Postal Code' value={formData.zip} error={formErrors.zip} refs={(input) => {this.zip = input}}/>
-        <CelSelect type='country' field='country' labelText='Country' showCountryFlag hideCallingCodes value={formData.country} error={formErrors.country} margin='0 0 0 0'/>
+        <CelInput type="text" field="street" placeholder='Address' value={formData.street} error={formErrors.street} returnKeyType={"next"} refs={(input) => { this.street = input }} blurOnSubmiting={false} onSubmitEditing={() => { this.flat.focus() }} />
+        <CelInput type="text" field="flatNumber" placeholder='Apartment number (optional)' value={formData.flatNumber} refs={(input) => { this.flat = input }} returnKeyType={"next"} blurOnSubmiting={false} onSubmitEditing={() => { this.city.focus() }} />
+        <CelInput type="text" field="city" placeholder='City' value={formData.city} error={formErrors.city} returnKeyType={"next"} refs={(input) => { this.city = input }} blurOnSubmiting={false} onSubmitEditing={() => { this.zip.focus() }} />
+        <CelInput type="text" field="zip" placeholder='ZIP/Postal Code' value={formData.zip} error={formErrors.zip} refs={(input) => { this.zip = input }} />
+        <CelSelect type='country' field='country' labelText='Country' showCountryFlag hideCallingCodes value={formData.country} error={formErrors.country} margin='0 0 0 0' />
 
         {formData.country && formData.country.name === 'United States' &&
-          <CelSelect type='state' field='state' labelText='State' value={formData.state} error={formErrors.state} margin='20 0 0 0'/>
+          <CelSelect type='state' field='state' labelText='State' value={formData.state} error={formErrors.state} margin='20 0 0 0' />
         }
 
-        <View style={{flex: 1, justifyContent: 'flex-end', marginTop: 30}}>
+        <View style={{ flex: 1, justifyContent: 'flex-end', marginTop: 30 }}>
           <CelButton onPress={this.submitAddressInfo} iconRight='IconArrowRight' loading={updatingAddressInfoInProgress}>
             Your Taxpayer ID
           </CelButton>
