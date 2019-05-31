0a9ea91049ce9c99c98b97a1abe4ad9181ff08f2
fj <filip.jovakaric@gmail.com>
2019-05-31 12:29:26 +0200

    CN-2842: update Borrow calculator

--- app/components/organisms/BorrowCalculator/BorrowCalculator.js
+++ app/components/organisms/BorrowCalculator/BorrowCalculator.js
@@ -3,6 +3,7 @@ import { View } from 'react-native'
 import PropTypes from "prop-types"
 import { connect } from 'react-redux'
 import { bindActionCreators } from "redux"
+import _ from "lodash";
 
 import testUtil from "../../../utils/test-util"
 import * as appActions from "../../../redux/actions"
@@ -20,7 +21,6 @@ import CelButton from "../../atoms/CelButton/CelButton"
 import Icon from "../../atoms/Icon/Icon"
 import { KYC_STATUSES } from "../../../constants/DATA";
 import { EMPTY_STATES } from "../../../constants/UI";
-
 
 
 @connect(
@@ -49,7 +49,6 @@ class BorrowCalculator extends Component {
       loanCompliance,
       formData,
       ltv,
-      walletSummary
     } = props
 
     const coinSelectItems = currencies
@@ -59,11 +58,9 @@ class BorrowCalculator extends Component {
         value: c.short
       }))
 
+
     this.state = {
       coinSelectItems,
-      rateFirst: false,
-      rateSecond: true,
-      rateThird: false
     }
 
     this.sliderItems = [
@@ -75,191 +72,98 @@ class BorrowCalculator extends Component {
 
     this.style = LoanCalculatorStyle()
 
-    this.coin = formData.coin || 'BTC'
-    this.loanDuration = formData.termOfLoan || 6
-    this.dollarAmount = formData.amount || 10000
+    props.actions.initForm({
+      coin: "BTC",
+      termOfLoan: 6,
+      amount: 10000,
+      ltv: ltv[0],
+    })
 
-    this.annualInterest = this.dollarAmount / (this.loanDuration / 12) * this.interestPercentage
+    // this.calculateLoanParams()
+  }
+
+  componentDidUpdate(prevProps) {
+    const { formData } = this.props;
+
+    if (!_.isEqual(formData, prevProps.formData)) {
+      // this.calculateLoanParams()
+    }
+  }
+
+  getPurposeSpecificProps = () => {
+    const { purpose, actions } = this.props;
+
+    const defaultProps = {
+      subtitle: 'You are not allowed to apply for a loan, but you can try our loan calculator.',
+      bottomHeading: 'Borrow dollars for your crypto',
+      bottomParagraph: 'Calculate your loan interest before you deposit coins',
+      buttonCopy: 'Deposit',
+      onPress: () => actions.navigateTo("Deposit", { coin: "CEL" }),
+    }
+
+    switch (purpose) {
+      case EMPTY_STATES.BORROW_NOT_ENOUGH_FUNDS:
+        return {
+          ...defaultProps,
+          subtitle: 'Calculate your loan interest',
+          bottomHeading: 'To apply for a loan you need only XXX ETH to deposit',
+          bottomParagraph: 'Deposit more coins to start your first loan application',
+          buttonCopy: 'Deposit coins',
+          onPress: '',
+        }
+
+      case EMPTY_STATES.NON_VERIFIED_BORROW:
+        return {
+          ...defaultProps,
+          subtitle: 'Calculate your interest before you verify your ID',
+          bottomHeading: 'Borrow dollars for your crypto',
+          bottomParagraph: 'Verify your identity yo start using your coins as collateral and get dollar loan at just XXX APR',
+          buttonCopy: 'Verify identity',
+          onPress: () => actions.navigateTo("KYCProfileDetails"),
+        }
+
+      case EMPTY_STATES.NON_MEMBER_CELPAY:
+        return {
+          ...defaultProps,
+          subtitle: 'Calculate your loan interest',
+          bottomHeading: 'Borrow dollars for your crypto',
+          bottomParagraph: 'Calculate your loan interest before you deposit coins',
+          buttonCopy: 'Deposit coins',
+          // onPress: '',
+        }
+
+      case EMPTY_STATES.COMPLIANCE:
+        return {
+          ...defaultProps,
+          subtitle: 'You are not allowed to apply for a loan because of local laws and regulations',
+          // bottomHeading: '',
+          // bottomParagraph: '',
+          buttonCopy: 'Go to Wallet',
+          onPress: 'WalletLanding',
+        }
+
+      default:
+        return defaultProps
+    }
+  }
+
+  calculateLoanParams = () => {
+    const { formData, currencies, walletSummary, ltv, purpose } = this.props
+
+    this.annualInterest = Number(formData.amount) / (formData.termOfLoan / 12) * formData.ltv.interest
     this.monthlyInterest = this.annualInterest / 12
-    this.totalInterest = this.monthlyInterest * this.loanDuration
+    this.totalInterest = this.monthlyInterest * formData.termOfLoan
 
-    this.collateralNeeded = (this.dollarAmount / (currencies.find(c => c.short === this.coin).market_quotes_usd.price)) / this.collateralPercentage
+    this.collateralNeeded = (Number(formData.amount) / (currencies.find(c => c.short === formData.coin).market_quotes_usd.price)) / formData.ltv.percent
 
+    if (purpose === EMPTY_STATES.BORROW_NOT_ENOUGH_FUNDS) {
+      this.bestLtv =  Math.max(ltv.map(x => x.percent))
 
-    this.cardStyle = this.style.cardStyle
-    this.selectedCardStyle = this.style.selectedCardStyle
-    this.selectedTextStyle = this.style.selectedTextStyle
-    this.percentageTextStyle = this.style.percentageTextStyle
-
-    this.interestPercentage = ltv[1].interest
-    this.collateralPercentage = ltv[1].percent
-
-    this.bestLtv =  Math.max(...ltv.map(x => x.percent))
-
-
-    this.biggestAmountCryptoUsd = Math.max(...walletSummary.coins.map(a => a.amount_usd))
-    this.arrayOfAmountUsd = walletSummary.coins.map(c => c.amount_usd)
-    this.indexOfLargestAmount = this.arrayOfAmountUsd.indexOf(
-      Math.max(...this.arrayOfAmountUsd)
-    )
-
-    // if (this.state.rateFirst === true) {
-    //   this.interestPercentage = ltv[0].interest
-    //   this.collateralPercentage = ltv[0].percent
-    // } else if (this.state.rateSecond === true) {
-    //   this.interestPercentage = ltv[1].interest
-    //   this.collateralPercentage = ltv[1].percent
-    // } else if (this.state.rateThird === true) {
-    //   this.interestPercentage = ltv[2].interest
-    //   this.collateralPercentage = ltv[2].percent
-    // }
-  }
-
-
-  componentDidMount() {
-    const { actions } = this.props
-    actions.updateFormField('termOfLoan', 6)
-  }
-
-  // handleCoinChange = (field, value) => {
-  //   const { onChange, actions, updateFormField } = this.props
-
-  //   actions.updateFormField(field, value)
-
-  //   if (onChange) {
-  //     this.onChange(field, value)
-  //   } else {
-  //     this.updateFormField(field, value)
-  //   }
-
-  //   this.formData = value
-
-  //   console.log('field: ',field);
-  //   console.log('value: ', value);
-
-
-  // }
-
-  changeSelectedLoan = (selectedStep) => {
-    const { updateFormField } = this.props;
-    updateFormField('termOfLoan', selectedStep)
-  }
-
-  pasteCodeHelperButton = () => (
-    <CelText weight='500' type={'H3'} color={STYLES.COLORS.DARK_GRAY3}>USD</CelText>
-  )
-
-  renderSubtitle = () => {
-    const { purpose } = this.props
-
-
-    switch (purpose) {
-
-      case EMPTY_STATES.BORROW_NOT_ENOUGH_FUNDS:
-        return 'Calculate your loan interest'
-
-      case EMPTY_STATES.NON_VERIFIED_BORROW:
-        return 'Calculate your interest before you verify your ID'
-
-      case EMPTY_STATES.NON_MEMBER_CELPAY:
-        return 'Calculate your loan interest'
-
-      case EMPTY_STATES.COMPLIANCE:
-        return 'You are not allowed to apply for a loan due compliance issues'
-
-      default:
-        return 'You are not allowed to apply for a loan, but you can try our loan calculator :)'
-    }
-  }
-
-  renderBottomTitle = () => {
-
-    const { purpose } = this.props
-
-    switch (purpose) {
-
-      case EMPTY_STATES.BORROW_NOT_ENOUGH_FUNDS:
-        return 'To apply for a loan you need only XXX ETH to deposit'
-
-      case EMPTY_STATES.NON_VERIFIED_BORROW:
-        return 'Borrow dollars for your crypto'
-
-      case EMPTY_STATES.NON_MEMBER_CELPAY:
-        return 'Borrow dollars for your crypto'
-
-      case EMPTY_STATES.COMPLIANCE:
-        return ''
-
-      default:
-        return ''
-    }
-  }
-
-  renderBottomCopy = () => {
-    const { purpose }  = this.props
-
-    switch (purpose) {
-
-      case EMPTY_STATES.BORROW_NOT_ENOUGH_FUNDS:
-        return 'Deposit more coins to start your first loan application'
-
-      case EMPTY_STATES.NON_VERIFIED_BORROW:
-        return 'Verify your identity yo start using your coins as collateral and get dollar loan at just XXX APR'
-
-      case EMPTY_STATES.NON_MEMBER_CELPAY:
-        return 'Calculate your loan interest before you deposit CEL'
-
-      case EMPTY_STATES.COMPLIANCE:
-        return ''
-
-      default:
-        return ''
-    }
-  }
-
-  renderBottomButton = () => {
-
-    const { purpose } = this.props
-
-    switch (purpose) {
-
-      case EMPTY_STATES.BORROW_NOT_ENOUGH_FUNDS:
-        return 'Deposit coins'
-
-      case EMPTY_STATES.NON_VERIFIED_BORROW:
-        return 'Verify identity'
-
-      case EMPTY_STATES.NON_MEMBER_CELPAY:
-        return 'Deposit coins'
-
-      case EMPTY_STATES.COMPLIANCE:
-        return ''
-
-      default:
-        return ''
-    }
-  }
-
-  renderNavigate = () => {
-
-    const { purpose } = this.props
-
-    switch (purpose) {
-
-      case EMPTY_STATES.BORROW_NOT_ENOUGH_FUNDS:
-        return 'Deposit'
-
-      case EMPTY_STATES.NON_VERIFIED_BORROW:
-        return 'KYCLanding'
-
-      case EMPTY_STATES.NON_MEMBER_CELPAY:
-        return `'Deposit', { coin: "CEL" }`
-
-      case EMPTY_STATES.COMPLIANCE:
-        return ''
-
-      default:
-        return ''
+      this.biggestAmountCryptoUsd = Math.max(walletSummary.coins.map(a => a.amount_usd))
+      this.arrayOfAmountUsd = walletSummary.coins.map(c => c.amount_usd)
+      this.indexOfLargestAmount = this.arrayOfAmountUsd.indexOf(
+        Math.max(this.arrayOfAmountUsd)
+      )
     }
   }
 
@@ -267,7 +171,6 @@ class BorrowCalculator extends Component {
     const style = LoanCalculatorStyle()
     const {
       coinSelectItems,
-      rateFirst
     } = this.state
 
     const {
@@ -276,6 +179,11 @@ class BorrowCalculator extends Component {
       ltv
      } = this.props
 
+    const purposeProps = this.getPurposeSpecificProps()
+
+    if (!formData.ltv) return null
+
+    // console.log({ formData })
 
     return (
       <RegularLayout style={style.container}>
@@ -290,7 +198,7 @@ class BorrowCalculator extends Component {
           type={'H4'}
           margin={'4 0 20 0'}
           weight={'300'}>
-          {this.renderSubtitle()}
+          { purposeProps.subtitle }
         </CelText>
         <Separator />
         <CelText
@@ -300,12 +208,13 @@ class BorrowCalculator extends Component {
           Enter loan amount in dollars
         </CelText>
         <CelInput
-          helperButton={this.pasteCodeHelperButton}
+          rightText="USD"
           field={'amount'}
           type={'number'}
           placeholder={'10.000'}
           keyboardType={'numeric'}
-          value={this.dollarAmount} />
+          value={formData.amount}
+        />
         <Card>
           <CelText
             align={'center'}
@@ -314,29 +223,27 @@ class BorrowCalculator extends Component {
             weight={'300'}>
             Choose your annual percentage rate
           </CelText>
-          <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={LoanCalculatorStyle.ltvWrapper}>
             {ltv.map(c =>
               (
                 <Card
-                size={'thirdExtra'}
-                margin='20 5 20 5'
-                noBorder
-                styles={ rateFirst ? this.selectedCardStyle : this.cardStyle }
-                onPress={() => {
-                  this.setState({
-                    rateFirst: true,
-                    rateSecond: false,
-                    rateThird: false
-                  })
-                }}>
-                <CelText
-                  align={'center'}
-                  weight='bold'
-                  color={STYLES.COLORS.MEDIUM_GRAY}
-                  style={ rateFirst ? this.selectedTextStyle : this.percentageTextStyle }>
-                  {formatter.percentageDisplay(c.interest)}
-                </CelText>
-              </Card>
+                  size={'thirdExtra'}
+                  margin='20 5 20 5'
+                  noBorder
+                  key={c.interest}
+                  styles={ formData.ltv.interest === c.interest ? this.selectedCardStyle : this.cardStyle }
+                  onPress={() => {
+                    actions.updateFormField('ltv', c)
+                  }}
+                >
+                  <CelText
+                    align={'center'}
+                    weight='bold'
+                    color={STYLES.COLORS.MEDIUM_GRAY}
+                    style={ formData.ltv.interest === c.interest ? this.selectedTextStyle : this.percentageTextStyle }>
+                    {formatter.percentageDisplay(c.interest)}
+                  </CelText>
+                </Card>
               ))
             }
           </View>
@@ -351,18 +258,21 @@ class BorrowCalculator extends Component {
             items={this.sliderItems}
             field="termOfLoan"
             value={formData.termOfLoan}
-            updateFormField={actions.updateFormField}/>
+            updateFormField={actions.updateFormField}
+          />
           <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between'  }}>
             <Card
               size={'halfExtra'}
               margin='20 10 20 5'
               padding='20 5 20 5'
-              color={STYLES.COLORS.LIGHT_GRAY}>
+              color={STYLES.COLORS.LIGHT_GRAY}
+            >
               <CelText
                 align={'center'}
                 weight='bold'
                 color={STYLES.COLORS.MEDIUM_GRAY}
-                type={'H2'}>
+                type={'H2'}
+              >
                 {formatter.usd(this.monthlyInterest)}
               </CelText>
               <CelText
@@ -404,7 +314,7 @@ class BorrowCalculator extends Component {
         </CelText>
         <View>
           <Icon
-            name={`Icon${this.coin}`}
+            name={`Icon${formData.coin}`}
             width='40'
             height='40'
             color={STYLES.COLORS.WHITE}
@@ -413,10 +323,9 @@ class BorrowCalculator extends Component {
             <SimpleSelect
               items={coinSelectItems}
               field='coin'
-              displayValue={this.coin}
-              value={formData.termOfLoan}
+              displayValue={formData.coin}
+              value={formData.coin}
               updateFormField={actions.updateFormField}
-              onChange={this.handleCoinChange}
               placeholder='Choose a coin'
             />
           </View>
@@ -431,7 +340,7 @@ class BorrowCalculator extends Component {
             weight='bold'
             color={STYLES.COLORS.MEDIUM_GRAY}
             type={'H2'}>
-            {formatter.crypto(this.collateralNeeded, this.coin)}
+            {formatter.crypto(this.collateralNeeded, formData.coin)}
           </CelText>
           <CelText
             align={'center'}
@@ -454,20 +363,23 @@ class BorrowCalculator extends Component {
               weight="bold"
               type="H2"
               align="center"
-              margin={'20 0 20 0'}>
-              {this.renderBottomTitle()}
+              margin={'20 0 20 0'}
+            >
+              { purposeProps.bottomHeading }
             </CelText>
             <CelText
               align={'center'}
               type={'H4'}
               margin={'4 0 20 0'}
-              weight={'300'}>
-              {this.renderBottomCopy()}
-              {/* Verify your identity to start using your coins as collateral and get a dollar loan at just {formatter.percentageDisplay(ltv[0].interest)} APR */}
+              weight={'300'}
+            >
+              { purposeProps.bottomParagraph }
             </CelText>
             <CelButton
-              onPress={() => actions.navigateTo(this.renderNavigate())}
-              margin='20 0 20 0'>{this.renderBottomButton()}
+              onPress={purposeProps.onPress}
+              margin='20 0 20 0'
+            >
+              { purposeProps.buttonCopy }
             </CelButton>
           </View>
       </RegularLayout>
--- app/components/organisms/BorrowCalculator/BorrowCalculator.styles.js
+++ app/components/organisms/BorrowCalculator/BorrowCalculator.styles.js
@@ -1,42 +1,44 @@
-import STYLES from '../../../constants/STYLES'
-import { getThemedStyle } from '../../../utils/styles-util'
+import STYLES from "../../../constants/STYLES";
+import { getThemedStyle } from "../../../utils/styles-util";
 
 const base = {
-    container: {
-      flex: 1
-    },
-    cardStyle: {
-      flex: 1,
-      borderWidth: 1,
-      backgroundColor: STYLES.COLORS.WHITE,
-      borderColor: STYLES.COLORS.DARK_GRAY3
-    },
-    selectedCardStyle: {
-      flex: 1,
-      borderWidth: 1,
-      backgroundColor: STYLES.COLORS.CELSIUS_BLUE,
-      borderColor: STYLES.COLORS.CELSIUS_BLUE,
-      color: STYLES.COLORS.WHITE
-    },
-    percentageTextStyle: {
-      color: STYLES.COLORS.DARK_GRAY
-    },
-    selectedTextStyle: {
-      color: STYLES.COLORS.WHITE
-    }
-}
+  container: {
+    flex: 1
+  },
+  cardStyle: {
+    flex: 1,
+    borderWidth: 1,
+    backgroundColor: STYLES.COLORS.WHITE,
+    borderColor: STYLES.COLORS.DARK_GRAY3
+  },
+  selectedCardStyle: {
+    flex: 1,
+    borderWidth: 1,
+    backgroundColor: STYLES.COLORS.CELSIUS_BLUE,
+    borderColor: STYLES.COLORS.CELSIUS_BLUE,
+    color: STYLES.COLORS.WHITE
+  },
+  percentageTextStyle: {
+    color: STYLES.COLORS.DARK_GRAY
+  },
+  selectedTextStyle: {
+    color: STYLES.COLORS.WHITE
+  },
+  ltvWrapper: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  }
+};
 
 const themed = {
-    light: {
-    },
+  light: {},
 
-    dark: {
-    },
+  dark: {},
 
-    celsius: {
-    }
-}
+  celsius: {}
+};
 
 const LoanCalculatorStyle = () => getThemedStyle(base, themed);
 
-export default LoanCalculatorStyle
+export default LoanCalculatorStyle;
