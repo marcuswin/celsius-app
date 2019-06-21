092d32528da01b729d4fdcc86b65718da5607e68
MVP Workshop <mvpworkshop@MVPs-iMac.local>
2019-06-21 17:05:02 +0200

    CN-3150: NetInfo package reimplemented

--- android/app/src/main/java/com/clearcelsius/MainApplication.java
+++ android/app/src/main/java/com/clearcelsius/MainApplication.java
@@ -3,6 +3,7 @@ package com.clearcelsius;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import io.branch.rnbranch.RNBranchPackage;
 import com.horcrux.svg.SvgPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NetInfoPackage(),
             new SplashScreenReactPackage(),
             new RNBranchPackage(),
             new SvgPackage(),
