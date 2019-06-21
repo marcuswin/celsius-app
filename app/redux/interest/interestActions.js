7a15f8cb4417fd04c789dce4fa4f8ddcab110c52
fj <filip.jovakaric@gmail.com>
2019-06-21 13:46:48 +0200

    CN-3157: fixing linting

--- app/redux/interest/interestActions.js
+++ app/redux/interest/interestActions.js
@@ -1,39 +1,39 @@
-import ACTIONS from '../../constants/ACTIONS';
-import API from '../../constants/API';
-import interestService from '../../services/interest-service';
-import { showMessage } from "../ui/uiActions";
-import { apiError, startApiCall } from "../api/apiActions";
+// import ACTIONS from '../../constants/ACTIONS';
+// import API from '../../constants/API';
+// import interestService from '../../services/interest-service';
+// import { showMessage } from "../ui/uiActions";
+// import { apiError, startApiCall } from "../api/apiActions";
 
 export {
-  getInterestChartData,
+  // getInterestChartData,
 }
 
 
-/**
- * Gets graph data for interest
- */
-function getInterestChartData(interval) {
-  return async dispatch => {
-    dispatch(startApiCall(API.GET_INTEREST_CHART_DATA));
-    try {
-      const res = await interestService.getInterestChartData(interval);
-      const chartData = res.data;
-      dispatch(getInterestChartDataSuccess(chartData));
-    } catch (err) {
-      dispatch(showMessage('error', err.msg));
-      dispatch(apiError(API.GET_INTEREST_CHART_DATA, err));
-    }
-  }
-}
-
-
-/**
- * TODO add JSDoc
- */
-function getInterestChartDataSuccess(chartData) {
-  return {
-    type: ACTIONS.GET_INTEREST_CHART_DATA_SUCCESS,
-    callName: API.GET_INTEREST_CHART_DATA,
-    chartData,
-  }
-}
+// /**
+//  * Gets graph data for interest
+//  */
+// function getInterestChartData(interval) {
+//   return async dispatch => {
+//     dispatch(startApiCall(API.GET_INTEREST_CHART_DATA));
+//     try {
+//       const res = await interestService.getInterestChartData(interval);
+//       const chartData = res.data;
+//       dispatch(getInterestChartDataSuccess(chartData));
+//     } catch (err) {
+//       dispatch(showMessage('error', err.msg));
+//       dispatch(apiError(API.GET_INTEREST_CHART_DATA, err));
+//     }
+//   }
+// }
+//
+//
+// /**
+//  * TODO add JSDoc
+//  */
+// function getInterestChartDataSuccess(chartData) {
+//   return {
+//     type: ACTIONS.GET_INTEREST_CHART_DATA_SUCCESS,
+//     callName: API.GET_INTEREST_CHART_DATA,
+//     chartData,
+//   }
+// }
