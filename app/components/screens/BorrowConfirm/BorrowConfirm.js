9ed3915baa75e8b018c4c1f98fe31a11106c0496
nemanja <nemanjakrstonic2@gmail.com>
2019-07-11 23:31:59 +0200

    CN-3344: Borrow Confirm Hardcoded values

--- app/components/screens/BorrowConfirm/BorrowConfirm.js
+++ app/components/screens/BorrowConfirm/BorrowConfirm.js
@@ -12,6 +12,7 @@ import RegularLayout from '../../layouts/RegularLayout/RegularLayout';
 import Separator from '../../atoms/Separator/Separator';
 import formatter from '../../../utils/formatter';
 import STYLES from '../../../constants/STYLES';
+import CelButton from '../../atoms/CelButton/CelButton';
 
 @connect(
   state => ({
@@ -34,8 +35,8 @@ class BorrowConfirm extends Component {
     return (
       <RegularLayout>
         <View>
-          <CelText type="H5" weight="300" align="center">You are about to borrow </CelText>
-          <CelText align="center" type="H1" weight="600" style={{ paddingBottom: 10 }}>{formatter.usd(formData.loanAmount, { precision: 0 })}</CelText>
+          <CelText type="H4" weight="300" align="center">You are about to borrow </CelText>
+          <CelText align="center" type="H0" weight="600" style={{ paddingBottom: 10 }}>{formatter.usd(formData.loanAmount, { precision: 0 })}</CelText>
           <View style={style.estimatedCollateral}>
             <CelText type="H6" weight="300" align="center" style={{ paddingBottom: 10 }}>Estimated collateral</CelText>
             <CelText type="H3" weight="600" align="center" style={{ paddingBottom: 10 }}>{formatter.percentage(formData.interest)}%</CelText>
@@ -45,71 +46,123 @@ class BorrowConfirm extends Component {
           </View>
 
           <View style={style.termLenght}>
-            <View>
+            <View style={{ flex: 1, alignItems: 'center' }}>
               <CelText type="H6" weight="300" color={STYLES.COLORS.MEDIUM_GRAY} style={{ paddingBottom: 10 }}>Term length</CelText>
               <CelText type="H4" weight="600" >{formData.termOfLoan} months</CelText>
             </View>
             <Separator vertical />
-            <View>
+            <View style={{ flex: 1, alignItems: 'center' }}>
               <CelText type="H6" weight="300" color={STYLES.COLORS.MEDIUM_GRAY} style={{ paddingBottom: 10 }}>Annual interest rate</CelText>
               <CelText type="H4" weight="600" >{formatter.percentage(formData.interest)}%</CelText>
             </View>
           </View>
 
           <View style={style.termLenght}>
-            <View>
+            <View style={{ flex: 1, alignItems: 'center' }}>
               <CelText type="H6" weight="300" color={STYLES.COLORS.MEDIUM_GRAY} style={{ paddingBottom: 10 }}>Monthly interest</CelText>
               <CelText type="H3" weight="600" align="center">{formatter.usd(formData.monthlyPayment)}</CelText>
             </View>
             <Separator vertical />
-            <View>
+            <View style={{ flex: 1, alignItems: 'center' }}>
               <CelText type="H6" weight="300" color={STYLES.COLORS.MEDIUM_GRAY} style={{ paddingBottom: 10 }}>Total interest</CelText>
-              <CelText type="H4" weight="600" >{formatter.percentage(formData.interest)}%</CelText>
+              <CelText type="H4" weight="600" color={STYLES.COLORS.CELSIUS_BLUE}>{formatter.percentage(formData.interest)}%</CelText>
             </View>
+          </View>
+
+
+          <View style={style.totalOfPayments}>
+            <CelText type='H6' weight='300'>Total of Payments</CelText>
+            <CelText type='H3' weight='600'>5,450 USDC</CelText>
+            <CelText type='H6' weight='300'>(Amount Borrowed + Total Interest)</CelText>
+            <Separator margin='10 0 10 0' />
+            <CelText type='H6' weight='300'>Number of Payments</CelText>
+            <CelText type='H3' weight='600'>12</CelText>
           </View>
 
           <View style={style.reduceInterest}>
 
             <View style={{ alignItems: 'center' }}>
-              <CelText type="H6" weight="300" color={STYLES.COLORS.LIGHT_GRAY}> Reduce your interest rate by</CelText>
-              <CelText type="H6" weight="300" color={STYLES.COLORS.LIGHT_GRAY}> XX% </CelText>
+              <CelText type="H6" weight="300" color={STYLES.COLORS.LIGHT_GRAY} style={{paddingBottom: 5}}> Reduce your interest rate by</CelText>
+              <CelText type="H3" weight="600" color={STYLES.COLORS.LIGHT_GRAY} style={{paddingBottom: 5}}> XX% </CelText>
               <CelText type="H6" weight="300" color={STYLES.COLORS.LIGHT_GRAY}> by paying out in CEL</CelText>
 
             </View>
             <Separator color={STYLES.COLORS.LIGHT_GRAY} margin='10 0 10 0' />
 
             <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-              <View style={{ flexDirection: 'column',  alignItems: 'center' }}>
+              <View style={{ flexDirection: 'column', alignItems: 'center' }}>
                 <CelText type="H6" weight="300" color={STYLES.COLORS.LIGHT_GRAY}>Monthly interest</CelText>
-                <CelText color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
+                <CelText type="H3" weight="600" color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
               </View>
               <Separator vertical color={STYLES.COLORS.LIGHT_GRAY} />
-              <View style={{ alignItems: 'center'}}>
+              <View style={{ alignItems: 'center' }}>
                 <CelText type="H6" weight="300" color={STYLES.COLORS.LIGHT_GRAY}>Monthly interest</CelText>
-                <CelText color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
-              </View>
-
-            </View>
-
-            <View style={{ flexDirection: 'row', justifyContent: 'space-around', backgroundColor: 'whtie' }}>
-              <View style={{ flexDirection: 'column',  alignItems: 'center' }}>
-                <CelText type="H6" weight="300" color={STYLES.COLORS.LIGHT_GRAY}>Monthly interest</CelText>
-                <CelText color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
-              </View>
-              <Separator vertical color={STYLES.COLORS.LIGHT_GRAY} />
-              <View style={{ alignItems: 'center'}}>
-                <CelText type="H6" weight="300" color={STYLES.COLORS.LIGHT_GRAY}>Monthly interest</CelText>
-                <CelText color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
+                <CelText type="H3" weight="600" color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
               </View>
             </View>
 
+
+            <View style={style.reduceInterestInnerBox}>
+              <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center' }}>
+                <CelText type="H6" weight="300" align='center' color={STYLES.COLORS.LIGHT_GRAY} margin='0 0 5 0'>Original Monthly Interest</CelText>
+                <CelText type="H3" weight="600" color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
+              </View>
+
+              <Separator vertical color={STYLES.COLORS.LIGHT_GRAY} margin='0 8 0 8' />
+
+              <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center' }}>
+                <CelText type="H6" weight="300" align='center' color={STYLES.COLORS.LIGHT_GRAY} margin='0 0 5 0'>Discounted Monthly Interest</CelText>
+                <CelText type="H3" weight="600" color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
+              </View>
+            </View>
+
+            <View style={style.reduceInterestInnerBox}>
+              <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center' }}>
+                <CelText type="H6" weight="300" align='center' color={STYLES.COLORS.LIGHT_GRAY} margin='0 0 5 0'>Original Total Interest</CelText>
+                <CelText type="H3" weight="600" color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
+              </View>
+
+              <Separator vertical color={STYLES.COLORS.LIGHT_GRAY} margin='0 8 0 8' />
+
+              <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center' }}>
+                <CelText type="H6" weight="300" align='center' color={STYLES.COLORS.LIGHT_GRAY} margin='0 0 5 0'>Discounted Total Interest</CelText>
+                <CelText type="H3" weight="600" color={STYLES.COLORS.LIGHT_GRAY}>XX.X CEL</CelText>
+              </View>
+            </View>
 
             <View>
               <CelText type="H6" weight="300" italic color={STYLES.COLORS.LIGHT_GRAY}>This rate is subject to change based on your loyalty level at the time of payment.</CelText>
             </View>
 
-
           </View>
+          <View style={style.estimatedCollateral}>
+            <CelText type="H6" weight="300" align="center" style={{ paddingBottom: 10 }}>BTC margin call at</CelText>
+            <CelText type="H3" weight="600" align="center" style={{ paddingBottom: 10 }}>{formatter.percentage(formData.interest)}%</CelText>
+            <View style={style.collateralInnerBox}>
+              <CelText type="H6" weight="300" align="left">If BTC drops below $XXX, you will receive a notification to review your borrowing options.</CelText>
+            </View>
+          </View>
+
+          <View style={style.estimatedCollateral}>
+            <CelText type="H6" weight="300" align="center" style={{ paddingBottom: 10 }}>Liquidation at</CelText>
+            <CelText type="H3" weight="600" align="center" style={{ paddingBottom: 10 }}>{formatter.percentage(formData.interest)}%</CelText>
+            <View style={style.collateralInnerBox}>
+              <CelText type="H6" weight="300" align="left">If BTC drops below $xxx we will sell some of your collateral to cover the margin.</CelText>
+            </View>
+          </View>
+
+          <View style={style.termLenght}>
+            <View style={{ flex: 1, alignItems: 'center' }}>
+              <CelText type="H6" weight="300" color={STYLES.COLORS.MEDIUM_GRAY} style={{ paddingBottom: 10 }}>First payment due</CelText>
+              <CelText type="H4" weight="600" >{formData.termOfLoan} months</CelText>
+            </View>
+            <Separator vertical />
+            <View style={{ flex: 1, alignItems: 'center' }}>
+              <CelText type="H6" weight="300" color={STYLES.COLORS.MEDIUM_GRAY} style={{ paddingBottom: 10 }}>Maturity date</CelText>
+              <CelText type="H4" weight="600" >{formatter.percentage(formData.interest)}%</CelText>
+            </View>
+          </View>
+          <CelButton> Request loan</CelButton>
         </View>
       </RegularLayout>
     );
--- app/components/screens/BorrowConfirm/BorrowConfirm.styles.js
+++ app/components/screens/BorrowConfirm/BorrowConfirm.styles.js
@@ -1,4 +1,4 @@
-// import STYLES from '../../../constants/STYLES';
+import { Platform } from 'react-native';
 import { getThemedStyle } from '../../../utils/styles-util';
 import STYLES from '../../../constants/STYLES';
 
@@ -12,6 +12,18 @@ const base = {
         backgroundColor: STYLES.COLORS.WHITE,
         borderRadius: 8,
         marginBottom: 15,
+        ...Platform.select({
+            android: {
+                borderColor: '#E9E9E9',
+                borderTopWidth: 0.2,
+                borderLeftWidth: 0.2,
+                borderRightWidth: 0.5,
+                borderBottomWidth: 2,
+            },
+            ios: {
+                ...STYLES.SHADOW_STYLES,
+            }
+        })
     },
     collateralInnerBox: {
         backgroundColor: STYLES.COLORS.LIGHT_GRAY,
@@ -20,15 +32,25 @@ const base = {
         borderRadius: 8
     },
     termLenght: {
-        flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-around',
+        backgroundColor: STYLES.COLORS.WHITE,
         paddingHorizontal: 10,
         paddingVertical: 10,
-        backgroundColor: STYLES.COLORS.WHITE,
         borderRadius: 8,
         marginBottom: 15,
-        
+        ...Platform.select({
+            android: {
+                borderColor: '#E9E9E9',
+                borderTopWidth: 0.2,
+                borderLeftWidth: 0.2,
+                borderRightWidth: 0.5,
+                borderBottomWidth: 2,
+            },
+            ios: {
+                ...STYLES.SHADOW_STYLES,
+            }
+        })
     },
     reduceInterest: {
         paddingHorizontal: 10,
@@ -36,14 +58,72 @@ const base = {
         backgroundColor: STYLES.COLORS.CELSIUS_BLUE,
         borderRadius: 8,
         marginBottom: 15,
+        ...Platform.select({
+            android: {
+                borderColor: '#E9E9E9',
+                borderTopWidth: 0.2,
+                borderLeftWidth: 0.2,
+                borderRightWidth: 0.5,
+                borderBottomWidth: 2,
+            },
+            ios: {
+                ...STYLES.SHADOW_STYLES,
+            }
+        })
+    },
+    reduceInterestInnerBox: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: STYLES.COLORS.WHITE_OPACITY1,
+        borderRadius: 8,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        marginVertical: 10,
+    },
+    totalOfPayments: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: STYLES.COLORS.WHITE,
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        marginBottom: 15,
+        ...Platform.select({
+            android: {
+                borderColor: '#E9E9E9',
+                borderTopWidth: 0.2,
+                borderLeftWidth: 0.2,
+                borderRightWidth: 0.5,
+                borderBottomWidth: 2,
+            },
+            ios: {
+                ...STYLES.SHADOW_STYLES,
+            }
+        })
+
     }
 }
 
 const themed = {
     light: {
+        container: {
+            backgroundColor: STYLES.COLORS.CELSIUS_BLUE,
+            borderColor: STYLES.COLORS.CELSIUS_BLUE
+        },
     },
-
     dark: {
+        estimatedCollateral: {
+            backgroundColor: STYLES.COLORS.DARK_HEADER
+        },
+        collateralInnerBox: {
+            backgroundColor: STYLES.COLORS.DARK_BACKGROUND
+        },
+        termLenght: {
+            backgroundColor: STYLES.COLORS.DARK_HEADER
+        },
+        totalOfPayments: {
+            backgroundColor: STYLES.COLORS.DARK_HEADER
+        }
     },
 
     celsius: {
