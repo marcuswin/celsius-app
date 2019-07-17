33c94a9a558be74c29873ef9ad511498e99d9f6d
fj <filip.jovakaric@gmail.com>
2019-07-17 15:21:19 +0200

    CN-2886: adding expo util

---
+++ app/utils/expo-util.js
@@ -0,0 +1,47 @@
+// import {
+//   Svg,
+//   Constants,
+//   Segment,
+//   Location,
+//   Permissions,
+//   BarCodeScanner,
+//   AppLoading,
+//   BlurView,
+//   Facebook,
+//   GoogleSignIn,
+//   Google,
+//   Asset,
+//   Font,
+//   Notifications,
+//   Camera,
+//   ImageManipulator,
+//   ImagePicker,
+//   Contacts,
+//   registerRootComponent,
+// } from 'expo';
+
+export {
+  Svg,
+  Constants,
+  Segment,
+  Location,
+  Permissions,
+  BarCodeScanner,
+  AppLoading,
+  BlurView,
+  Facebook,
+  GoogleSignIn,
+  Google,
+  Asset,
+  Font,
+  Notifications,
+  Camera,
+  ImageManipulator,
+  ImagePicker,
+  Contacts,
+  registerRootComponent,
+} from "expo";
+
+// export * from "expo"
+
+
