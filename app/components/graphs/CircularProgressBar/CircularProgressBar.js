bc2818ecaa34c3023f289acb2a37106950cbcd25
Nikola Sabelnik <sabelnikola@gmail.com>
2019-07-17 17:19:28 +0200

    CN-3336 Prop amendments

--- app/components/graphs/CircularProgressBar/CircularProgressBar.js
+++ app/components/graphs/CircularProgressBar/CircularProgressBar.js
@@ -8,6 +8,7 @@ import { heightPercentageToDP } from "../../../utils/styles-util";
 import STYLES from "../../../constants/STYLES";
 import CelText from "../../atoms/CelText/CelText";
 
+const height = heightPercentageToDP("22.5%")
 
 const { Surface, Group, Shape } = ART;
 
@@ -16,11 +17,8 @@ class CircularProgressBar extends Component {
   static propTypes = {
     amountPaid: PropTypes.number,
     amountLoaned: PropTypes.number,
-    width: PropTypes.number,
   };
-  static defaultProps = {
-    width: heightPercentageToDP("22.5%")
-  };
+  static defaultProps = {};
 
   constructor(props) {
     super(props);
@@ -29,19 +27,19 @@ class CircularProgressBar extends Component {
 
   render() {
     const style = CircularProgressBarStyles();
-    const {width, amountLoaned, amountPaid} = this.props;
+    const {amountLoaned, amountPaid} = this.props;
     const percentage = amountPaid * 100 / amountLoaned;
     const section = d3.pie().value(l => l.percent)({});
     const path = d3.arc()
       .startAngle(0)
       .endAngle(Math.PI * 2 * percentage / 100)
-      .outerRadius(width / 2.5) // must be less than 1/2 the chart's height/width
-      .innerRadius(width / 2.3); // the size of the inner 'donut' whitespace
+      .outerRadius(height / 2.5) // must be less than 1/2 the chart's height/width
+      .innerRadius(height / 2.3); // the size of the inner 'donut' whitespace
 
     return (
       <View>
-        <Surface width={width} height={width}>
-          <Group x={width / 2} y={width / 2}>
+        <Surface width={height} height={height}>
+          <Group x={height / 2} y={height / 2}>
             {
               section.map(sec => (
                 <Shape
--- app/components/organisms/LoanOverviewCard/LoanOverviewCard.styles.js
+++ app/components/organisms/LoanOverviewCard/LoanOverviewCard.styles.js
@@ -1,5 +1,5 @@
 // import STYLES from '../../../constants/STYLES';
-import { getThemedStyle, widthPercentageToDP } from "../../../utils/styles-util";
+import { getThemedStyle, heightPercentageToDP, widthPercentageToDP } from "../../../utils/styles-util";
 
 const base = {
   container: {
@@ -10,7 +10,7 @@ const base = {
   interest: { flexDirection: "row", padding: 12 },
   status: { flexDirection: "row", alignItems: "center" },
   info: { alignItems: "center", padding: 12 },
-  progress: {position: "absolute", left: widthPercentageToDP("30%")}
+  progress: {position: "absolute", left: widthPercentageToDP("29%"), top: heightPercentageToDP("1.3%")}
 };
 
 const themed = {
