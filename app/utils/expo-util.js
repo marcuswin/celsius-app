9c41a2bfd0def2a1d877a1e95d093bb4773ba941
fj <filip.jovakaric@gmail.com>
2019-07-18 10:50:35 +0200

    CN-2886: adding unimodules to project

--- app/utils/expo-util.js
+++ app/utils/expo-util.js
@@ -1,25 +1,21 @@
-import {
-  Svg,
-  Constants,
-  Segment,
-  Location,
-  Permissions,
-  BarCodeScanner,
-  AppLoading,
-  BlurView,
-  Facebook,
-  GoogleSignIn,
-  Google,
-  Asset,
-  Font,
-  Notifications,
-  Camera,
-  ImageManipulator,
-  ImagePicker,
-  Contacts,
-  registerRootComponent,
-  SecureStore,
-} from 'expo';
+import { AppLoading, Google, Notifications, registerRootComponent } from 'expo';
+
+import * as SecureStore from 'expo-secure-store';
+import * as Contacts from 'expo-contacts';
+import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
+import { Camera } from 'expo-camera';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import * as GoogleSignIn from 'expo-google-sign-in';
+import * as Facebook from 'expo-facebook';
+import { BlurView } from 'expo-blur';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
+import * as Segment from 'expo-analytics-segment';
+import Constants from 'expo-constants';
+import * as Svg from 'react-native-svg';
 
 export {
   Svg,
@@ -43,11 +39,3 @@ export {
   registerRootComponent,
   SecureStore,
 }
-
-// export * from "expo"
-
-// import * as Expo from "expo";
-//
-// const newExpo = { ...Expo }
-//
-// export { ...Expo }
