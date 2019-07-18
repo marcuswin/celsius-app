b909db304d536ef22d4933ed86b1ed252eef75ce
nemanja <nemanjakrstonic2@gmail.com>
2019-07-18 23:10:21 +0200

    CN-3335: Loan Payment List
    Hard-coded values

--- app/components/screens/LoanPaymentList/LoanPaymentList.js
+++ app/components/screens/LoanPaymentList/LoanPaymentList.js
@@ -5,12 +5,11 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
 
 import * as appActions from "../../../redux/actions";
-// import LoanPaymentListStyle from "./LoanPaymentList.styles";
+import LoanPaymentListStyle from "./LoanPaymentList.styles";
 import CelText from '../../atoms/CelText/CelText';
 import RegularLayout from '../../layouts/RegularLayout/RegularLayout';
 import Separator from '../../atoms/Separator/Separator';
 import STYLES from '../../../constants/STYLES';
-// import LoanPaymentListStyle from './LoanPaymentList.styles';
 import Card from '../../atoms/Card/Card';
 
 @connect(
@@ -25,47 +24,69 @@ import Card from '../../atoms/Card/Card';
         dueDate: '12.3.5004',
       },
       {
-        amountToPay: 1000,
+        amountToPay: 2000,
+        amountPaid: 2000,
+        type: 'PaymentType.MONTHLY_INTEREST',
+        dueDate: '12.4.5000',
+      },
+      {
+        amountToPay: 2000,
         amountPaid: 1000,
         type: 'PaymentType.MONTHLY_INTEREST',
-        dueDate: '12.3.5000',
+        dueDate: '12.5.5000',
       },
       {
-        amountToPay: 1000,
+        amountToPay: 3000,
         amountPaid: 500,
         type: 'PaymentType.MONTHLY_INTEREST',
-        dueDate: '12.3.5000',
-      },
-      {
-        amountToPay: 1000,
-        amountPaid: 400,
-        type: 'PaymentType.MONTHLY_INTEREST',
-        dueDate: '12.3.5000',
+        dueDate: '12.6.5000',
       },
       {
         amountToPay: 1000,
         amountPaid: 0,
         type: 'PaymentType.MONTHLY_INTEREST',
-        dueDate: '12.3.5001',
+        dueDate: '12.7.5001',
       },
       {
         amountToPay: 1000,
         amountPaid: 0,
         type: 'PaymentType.MONTHLY_INTEREST',
-        dueDate: '12.3.5001',
+        dueDate: '12.8.5001',
+      },
+      {
+        amountToPay: 1000,
+        amountPaid: 0,
+        type: 'PaymentType.MONTHLY_INTEREST',
+        dueDate: '12.9.5001',
+      },
+      {
+        amountToPay: 1000,
+        amountPaid: 0,
+        type: 'PaymentType.MONTHLY_INTEREST',
+        dueDate: '12.8.5001',
+      },
+      {
+        amountToPay: 1000,
+        amountPaid: 0,
+        type: 'PaymentType.MONTHLY_INTEREST',
+        dueDate: '12.9.5001',
+      },
+      {
+        amountToPay: 1000,
+        amountPaid: 0,
+        type: 'PaymentType.MONTHLY_INTEREST',
+        dueDate: '12.10.5001',
       },
       {
         amountToPay: "last payment",
         amountPaid: 12,
         type: 'PaymentType.MONTHLY_INTEREST',
-        dueDate: '12.3.5000',
+        dueDate: '12.11.5000',
       }
     ]
   }),
   dispatch => ({ actions: bindActionCreators(appActions, dispatch) }),
 )
-
-
 
 class LoanPaymentList extends Component {
   static propTypes = {};
@@ -79,66 +100,68 @@ class LoanPaymentList extends Component {
   // isPaid represents difference between amountPaid and amoutToPay 
   renderFuturePayments = () => {
     const { loans } = this.props
-
+    const style = LoanPaymentListStyle()
     const futurePayments = loans.slice(0, -1)
     // const nextPayment = loans[0]
 
     const isNotPaid = futurePayments.filter(p => p.amountPaid < p.amountToPay).map((p, index) => (
-      <View style={{ backgroundColor: index === 0 ? STYLES.COLORS.MEDIUM_GRAY3 : STYLES.COLORS.WHITE_OPACITY1, borderRadius: 8 }}>
-        <View style={{ flexDirection: 'row', paddingHorizontal: 10, justifyContent: 'space-between', alignItems: 'center', marginVertical: 10 }}>
+      <View style={{ height: 50, marginVertical: 0, backgroundColor: index === 0 ? STYLES.COLORS.MEDIUM_GRAY3 : STYLES.COLORS.LIGHT_GRAY, borderRadius: 8 }}>
+        <View style={style.upcomintPayment}>
           <CelText weight='600' type='H3'>{p.amountToPay}</CelText>
           <CelText weight='300' type='H6'>{p.dueDate}</CelText>
         </View>
-        {index === 0 ? null :
+        {index === 0 || index === futurePayments.length - 3 ? null :
           <Separator />}
       </View>
-    )
-    )
-    return (
+      )
+      )
+      return (
       <View>
-        <View style={{ postion: 'abosulte', alignSelf: 'center', backgroundColor: STYLES.COLORS.MEDIUM_GRAY1, width: 120, height: 20, borderRadius: 4 }}>
-          <CelText align='center' type='H6'>Next payment</CelText>
-        </View>
-        {isNotPaid}
-      </View>
-    )
-  }
-
-  renderPayments = () => {
-    const { loans } = this.props
-    // const style = LoanPaymentListStyle()
-    const principalPayout = loans[loans.length - 1]
-
-    return (
-      <View>
-        {this.renderFuturePayments()}
-        <View style={{ alignSelf: 'center', backgroundColor: STYLES.COLORS.MEDIUM_GRAY1, width: 120, height: 20, borderRadius: 4 }}>
-          <CelText align='center' type='H6'>Principal payout</CelText>
-        </View>
-        <Card color={STYLES.COLORS.GREEN} >
-          <View style={{ flexDirection: 'row', paddingHorizontal: 10, justifyContent: 'space-between', alignItems: 'center' }}>
-            <CelText color='white' weight='600' type='H3'>{principalPayout.amountToPay}</CelText>
-            <CelText color='white' weight='300' type='H6'>{principalPayout.dueDate}</CelText>
+          <View style={style.nextPayment}>
+            <CelText align='center' type='H6'>Next payment</CelText>
           </View>
-        </Card>
+          <View>
+            {isNotPaid}
+          </View>
+        </View>
+        )
+      }
+    
+  renderPayments = () => {
+    const {loans} = this.props
+        const style = LoanPaymentListStyle()
+        const principalPayout = loans[loans.length - 1]
+    
+        return (
+      <View>
+          {this.renderFuturePayments()}
+          <View style={style.principalPayment}>
+            <CelText align='center' type='H6'>Principal payout</CelText>
+          </View>
+          <Card color={STYLES.COLORS.GREEN} >
+            <View style={style.lastPayment}>
+              <CelText color='white' weight='600' type='H3'>{principalPayout.amountToPay}</CelText>
+              <CelText color='white' weight='300' type='H6'>{principalPayout.dueDate}</CelText>
+            </View>
+          </Card>
 
-      </View>
-    )
-  }
-
+        </View>
+        )
+      }
+    
   render() {
     // const style = LoanPaymentListStyle();
     return (
       <RegularLayout>
-        <View style={{ marginHorizontal: 15 }}>
-          <CelText weight='500' type='H6'>Upcoming Payments</CelText>
-          <View style={{ marginTop: 30 }}>
-            {this.renderPayments()}
+          <View style={{ marginHorizontal: 15 }}>
+            <CelText weight='500' type='H6'>Upcoming Payments</CelText>
+            <View style={{ marginTop: 15 }}>
+              {this.renderPayments()}
+            </View>
           </View>
-        </View>
-      </RegularLayout>
-    );
-  }
-}
-
-export default LoanPaymentList
+        </RegularLayout>
+        );
+      }
+    }
+    
+    export default LoanPaymentList
--- app/components/screens/LoanPaymentList/LoanPaymentList.styles.js
+++ app/components/screens/LoanPaymentList/LoanPaymentList.styles.js
@@ -5,14 +5,35 @@ const base = {
     container: {
         flex: 1
     },
-    info: {
+    upcomintPayment: {
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 10,
+    },
+    nextPayment: {
+        top: 10,
+        zIndex: 1,
         alignSelf: 'center',
-        borderWidth: 4,
-        borderRadius: 5,
-        width: 80,
-        height: 20,
         backgroundColor: STYLES.COLORS.LIGHT_GRAY,
-        borderColor: STYLES.COLORS.LIGHT_GRAY,
+        width: 120,
+        height: 20,
+        borderRadius: 4
+    },
+    principalPayment: {
+        top: 20,
+        zIndex: 1,
+        alignSelf: 'center',
+        backgroundColor: STYLES.COLORS.LIGHT_GRAY,
+        width: 120,
+        height: 20,
+        borderRadius: 4
+    },
+    lastPayment: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     }
 
 }
