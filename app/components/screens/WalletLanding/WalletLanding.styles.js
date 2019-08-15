b13bca18e46e34ede163ce7b578d3361bc0b14d0
Nikola <30319637+ShamanShaulic@users.noreply.github.com>
2019-08-15 11:19:26 +0200

    CN-3610 Refactor wallet landing screen (#225)
    * CN-3610 Refactor wallet landing screen
    
    * CN-3610: rename styles

--- app/components/screens/WalletLanding/WalletLanding.styles.js
+++ app/components/screens/WalletLanding/WalletLanding.styles.js
@@ -1,51 +1,25 @@
-import { getThemedStyle, widthPercentageToDP } from '../../../utils/styles-util';
+import { getThemedStyle } from '../../../utils/styles-util';
 
 
 const base = {
-  coinCardContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between'
-  },
-  addMoreCoinsGrid: {
-    borderWidth: 1,
-    borderStyle: 'dashed',
-    borderRadius: 10,
-    width: widthPercentageToDP("50%") - 28, // same width as Card.styles.js style half
-    minHeight: 130,
-    borderColor: 'gray',
-    marginVertical: 8,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  addMoreCoinsList: {
-    borderWidth: 1,
-    borderStyle: 'dashed',
-    borderRadius: 10,
-    width: '100%',
-    height: 80,
-    borderColor: 'gray',
-    marginVertical: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  coinsTextGrid: {
-    marginTop: '35%',
-    alignItems: 'center',
-    alignContent: 'center',
-    justifyContent: 'space-between'
-
-  },
-
-  coinsTextList: {
-    marginTop: '15%',
-    alignItems: 'center',
-    alignContent: 'center',
-    // justifyContent: 'space-between'
-  }
+  // coinsTextGrid: {
+  //   marginTop: '35%',
+  //   alignItems: 'center',
+  //   alignContent: 'center',
+  //   justifyContent: 'space-between'
+  //
+  // },
+  //
+  // coinsTextList: {
+  //   marginTop: '15%',
+  //   alignItems: 'center',
+  //   alignContent: 'center',
+  //   // justifyContent: 'space-between'
+  // },
+  depositWrapper: { flexDirection: 'row', justifyContent: 'space-between' },
+  depositedCoins: { alignSelf: 'center', marginTop: 20 },
+  buttonWrapper: { flexDirection: 'row', marginTop: 20 },
+  listView: { marginLeft: 16 }
 }
 
 const themed = {
