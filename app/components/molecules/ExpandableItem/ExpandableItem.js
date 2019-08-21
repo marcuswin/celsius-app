05fb4ddc12237eb4c835e91ea0a0b0372947ab84
StefanBurscher <stefan.burscher@gmail.com>
2019-08-21 13:16:20 +0200

    CN-3636: Delete deprecated files

--- app/components/molecules/ExpandableItem/ExpandableItem.js
+++ app/components/molecules/ExpandableItem/ExpandableItem.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { View, TouchableOpacity } from 'react-native';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { View, TouchableOpacity } from "react-native";
 
 import ExpandableItemStyle from "./ExpandableItem.styles";
 import CelText from "../../atoms/CelText/CelText";
 import Icon from "../../atoms/Icon/Icon";
-import { getTheme } from '../../../utils/styles-util'
+import { getTheme } from "../../../utils/styles-util";
 import { THEMES } from "../../../constants/UI";
-import STYLES from '../../../constants/STYLES';
+import STYLES from "../../../constants/STYLES";
 
 class ExpandableItem extends Component {
-
   static propTypes = {
     heading: PropTypes.string
   };
-  static defaultProps = {
-  }
+  static defaultProps = {};
 
   constructor(props) {
     super(props);
@@ -26,54 +24,49 @@ class ExpandableItem extends Component {
   }
 
   renderSeparator = () => {
-    const style = ExpandableItemStyle()
-    const { isExpanded } = this.state
-    const theme = getTheme()
-    const { heading } = this.props
-
+    const style = ExpandableItemStyle();
+    const { isExpanded } = this.state;
+    const theme = getTheme();
+    const { heading } = this.props;
 
     return (
       <TouchableOpacity
-      style={ style.container }
-      onPress={ () => this.setState({ isExpanded: !isExpanded }) }
-    >
-      <View style={ style.leftSegment }/>
-      <View style={ style.left } />
-
-      <CelText
-        align={'center'}
-        style={ style.centralText }
-        allCaps
+        style={style.container}
+        onPress={() => this.setState({ isExpanded: !isExpanded })}
       >
-        { heading }
-      </CelText>
+        <View style={style.leftSegment} />
+        <View style={style.left} />
 
-      <View style={ style.right } />
-      <Icon
-        name={ isExpanded ? 'Collapse' : 'Expand' }
-        height='21'
-        width='21'
-        fill={ theme === THEMES.DARK ? STYLES.COLORS.WHITE_OPACITY5 : STYLES.COLORS.GRAY }
-      />
-    </TouchableOpacity>)
-  }
+        <CelText align={"center"} style={style.centralText} allCaps>
+          {heading}
+        </CelText>
+
+        <View style={style.right} />
+        <Icon
+          name={isExpanded ? "Collapse" : "Expand"}
+          height="21"
+          width="21"
+          fill={
+            theme === THEMES.DARK
+              ? STYLES.COLORS.WHITE_OPACITY5
+              : STYLES.COLORS.GRAY
+          }
+        />
+      </TouchableOpacity>
+    );
+  };
 
   render() {
-    const { children, heading } = this.props
-    const { isExpanded } = this.state
+    const { children, heading } = this.props;
+    const { isExpanded } = this.state;
 
     return (
       <>
-        { heading && //
-          this.renderSeparator()
-        }
-        { isExpanded &&
-        <View>
-          { children }
-        </View> }
+        {heading && this.renderSeparator()}
+        {isExpanded && <View>{children}</View>}
       </>
-    )
+    );
   }
 }
 
-export default ExpandableItem
+export default ExpandableItem;
