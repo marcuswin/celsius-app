375126dbb1ee3f4a06a2654e71e4536f42c9b55c
nemanja <nemanjakrstonic2@gmail.com>
2019-08-26 18:06:08 +0200

    CN-3564 DRAFT Modal Scroll

--- app/components/organisms/WithdrawInfoModal/WithdrawInfoModal.js
+++ app/components/organisms/WithdrawInfoModal/WithdrawInfoModal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
-import { View, ScrollView, TouchableOpacity } from "react-native";
+import { Animated, View, ScrollView, TouchableOpacity } from "react-native";
 
 
 import formatter from "../../../utils/formatter";
@@ -10,6 +10,9 @@ import { MODALS } from "../../../constants/UI";
 import WithdrawInfoModalStyle from "./WithdrawInfoModal.styles";
 import CelButton from "../../atoms/CelButton/CelButton";
 import DotsBar from '../../atoms/DotsBar/DotsBar';
+import { widthPercentageToDP } from '../../../utils/styles-util';
+
+const cardWidth = widthPercentageToDP("70%");
 
 class WithdrawInfoModal extends Component {
   static propTypes = {
@@ -17,6 +20,7 @@ class WithdrawInfoModal extends Component {
     toggleKeypad: PropTypes.func,
     type: PropTypes.bool,
     withdrawalSettings: PropTypes.instanceOf(Object),
+    index: PropTypes.number,
   };
   static defaultProps = {}
 
@@ -26,28 +30,34 @@ class WithdrawInfoModal extends Component {
 
     this.state = {
       currentStep: 1,
+      xOffset: new Animated.Value(0),
       steps: [
         {
+          index: 1,
           image: require('../../../../assets/images/illustrations-v3/PolarBearHODL3x.png'),
           title: "Are you sure you want to withdraw CEL?",
           description: "A withdrawal could affect your membership interest rates. Rates are determined by your loyalty level: the amount of CEL you HODL vs. the amount of other assets you hold. Please view our loyalty program.",
         },
         {
+          index: 2,
           image: require('../../../../assets/images/illustrations-v3/PolarBearSad3x.png'),
-          title: `Immediate withdrawals under ${ withdrawalSettings && formatter.usd(withdrawalSettings.maximum_withdrawal_amount) }`,
+          title: `Immediate withdrawals under ${withdrawalSettings && formatter.usd(withdrawalSettings.maximum_withdrawal_amount)}`,
           description: "Celsius enables you to withdraw coins at any time. However, when exceeding this limit, withdrawals might be delayed for twenty-four (24) hours due to our security protocols.",
         },
         {
+          index: 3,
           image: require('../../../../assets/images/illustrations-v3/PolarBearFistUp3x.png'),
           title: "Don’t forget to check your withdrawal address",
           description: "Celsius uses a smart-contract based wallet for ETH and ERC20 coins, some wallets and exchanges (e.g. Bitfinex) do not support transactions from a smart-contract source. We recommend a one time test withdrawal with a small sum. Contact app@celsius.network if you are unsure.",
         },
         {
+          index: 4,
           image: require('../../../../assets/images/illustrations-v3/PolarBearFistUp3x.png'),
           title: "Transactions are safely secured with BitGo",
           description: "BitGo is a leading custodial service powering exchanges like Kraken and UPbit. Coins will be moved from time to time to exchanges or to Hedge funds borrowing coins in order to short the market, in this case, coins are converted to fiat and secured in an FDIC-insured bank account.",
         }
-      ]
+      ],
+
     };
   }
 
@@ -83,7 +93,6 @@ class WithdrawInfoModal extends Component {
     const { type } = this.props;
 
     const normalButton = [styles.modalButton]
-
     if (type) {
       if (currentStep !== 4) {
         normalButton.push(styles.whiteButton)
@@ -105,8 +114,34 @@ class WithdrawInfoModal extends Component {
 
   }
 
+  // scroll = () => {
+  //   // console.log('this.state.cu)')
+  //   this.setState(state => {
+  //     return {
+  //       currentStep: state.currentStep + 1,
+  //     }
+  //   })
+  // }
+
+  transitionAnimation = (index) => ({
+    transform: [
+      { perspective: 800 },
+      {
+        scale: this.state.xOffset.interpolate({
+          inputRange: [
+            (index - 1) * cardWidth,
+            index * cardWidth,
+            (index + 1) * cardWidth
+          ],
+          outputRange: [0.9, 1, 0.9],
+          extrapolate: "clamp"
+        })
+      }
+    ]
+  });
+
   renderStep() {
-    const { steps, currentStep } = this.state;
+    const { steps, xOffset } = this.state;
     const { type } = this.props
     const styles = WithdrawInfoModalStyle();
     const ButtonStyle = this.buttonColor;
@@ -114,10 +149,37 @@ class WithdrawInfoModal extends Component {
     const three = steps.slice(1, 4)
 
     return (
-      <View>
+      <View index={steps.index}>
         <ScrollView>
-          <CelText type='H2' weight='bold' style={styles.title}> {type ? steps[currentStep - 1].title : three[currentStep - 1].title}</CelText>
-          <CelText type='H4' style={styles.description}>{type ? steps[currentStep - 1].description : three[currentStep - 1].description}</CelText>
+          <Animated.ScrollView
+            scrollEventThrottle={16}
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { x: xOffset } } }],
+              { useNativeDriver: true }
+            )}
+            horizontal
+            pagingEnabled
+          >
+            {type ?
+              steps.map((step, index) => (
+                // console.log(currentStep, 'currentStep')
+                <Animated.View onScroll={this.scroll} key={index} style={[styles.screen, this.transitionAnimation(index)]}>
+                  <CelText type='H2' weight='bold' style={styles.title}> {step.title}</CelText>
+                  <CelText type='H4' style={styles.description}>{step.description}</CelText>
+                </Animated.View>
+              )
+              )
+              :
+              three.map((step, index) => (
+                // console.log(index, step)
+                // this.setState({ currentStep: step })
+                <Animated.View onScroll={this.scroll} key={index} style={[styles.screen, this.transitionAnimation(index)]}>
+                  <CelText type='H2' weight='bold' style={styles.title}> {step.title}</CelText>
+                  <CelText type='H4' style={styles.description}>{step.description}</CelText>
+                </Animated.View>
+              ))
+            }
+          </Animated.ScrollView>
           <View style={styles.button}>
             <ButtonStyle />
             <TouchableOpacity style={{ marginTop: 10 }} onPress={this.closeModalHandler}>
@@ -132,7 +194,7 @@ class WithdrawInfoModal extends Component {
   render() {
     const styles = WithdrawInfoModalStyle();
     const { steps, currentStep } = this.state;
-    const { type } = this.props;
+    const { type, index } = this.props;
     let numberOfSteps
 
     if (type) {
@@ -146,6 +208,7 @@ class WithdrawInfoModal extends Component {
         name={MODALS.WITHDRAW_INFO_MODAL}
         picture={steps[currentStep - 1].image}
         onClose={this.closeModalHandler}
+        index={index}
       >
         <View style={styles.wrapper}>
           <View style={styles.progressBar}>
