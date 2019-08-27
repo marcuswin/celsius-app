f9b0f999c279b49cd45807976478b641b53746ce
StefanBurscher <stefan.burscher@gmail.com>
2019-08-27 15:32:55 +0200

    pod remove branch

--- android/app/src/main/java/com/celsius/MainApplication.java
+++ android/app/src/main/java/com/celsius/MainApplication.java
@@ -3,6 +3,7 @@ package com.celsius;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.branch.rnbranch.RNBranchPackage;
 import info.applike.advertisingid.RNAdvertisingIdPackage;
 import com.kevinejohn.RNMixpanel.RNMixpanel;
 import com.microsoft.codepush.react.CodePush;
@@ -46,6 +47,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNBranchPackage(),
             new RNMixpanel(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new RNAppsFlyerPackage(),
