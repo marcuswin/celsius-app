a8eebccf4637e74bc4f971e2e95bd5219c9a9c45
nemanja <nemanjakrstonic2@gmail.com>
2019-09-25 16:41:50 +0200

    CN-3182-CN3806 Bug Fixes
    - Copy Change InfoBox
    - BalanceView Style change
    - USD formatter added

--- app/components/atoms/BalanceView/BalanceView.js
+++ app/components/atoms/BalanceView/BalanceView.js
@@ -3,29 +3,22 @@ import PropTypes from 'prop-types'
 import { View } from 'react-native'
 
 
-// import BalanceViewStyle from "./BalanceView.styles";
+import BalanceViewStyle from "./BalanceView.styles";
 import CelText from '../../atoms/CelText/CelText'
 import formatter from '../../../utils/formatter'
 
 const BalanceView = props => {
-  // const style = BalanceViewStyle();
+  const style = BalanceViewStyle();
   const { crypto, usd, coin } = props
 
   return (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 10,
-        marginBottom: 10
-      }}
-    >
-      <CelText align='right' type='H7' weight={"300"}>
+    <View style={style.view}>
+      <CelText align='right' type='H7' weight={"300"} style={style.text} >
         Balance:{' '}
         {`${formatter.getEllipsisAmount(
           formatter.removeDecimalZeros(crypto),
           -8
-        )} ${coin}  |  $ ${formatter.floor10(usd, -2)} USD`}
+        )} ${coin}  |   ${formatter.usd(formatter.floor10(usd, -2))} USD`}
       </CelText>
     </View>
   )
--- app/components/atoms/InfoBox/InfoBox.js
+++ app/components/atoms/InfoBox/InfoBox.js
@@ -31,8 +31,7 @@ class InfoBox extends Component {
     backgroundColor: PropTypes.string,
     triangle: PropTypes.bool,
     opened: PropTypes.bool,
-    explanationText: PropTypes.string,
-    wordsToHightlight: PropTypes.instanceOf(Array)
+    explanationText: PropTypes.oneOfType([PropTypes.string, PropTypes.component])
   }
 
   static defaultProps = {
@@ -56,21 +55,6 @@ class InfoBox extends Component {
     })
   }
 
-  renderHighlightedText() {
-    const { explanationText, wordsToHightlight } = this.props
-
-    const words = explanationText.split(' ')
-
-    const renderWord = (word) => {
-      if (wordsToHightlight.indexOf(word) > -1) {
-        return (<CelText color={'white'} weight={'700'}>{word} </CelText>)
-      }
-      return (<CelText color={'white'}>{word} </CelText>)
-
-    }
-    return (React.Children.map(words, renderWord))
-  }
-
   render() {
     const {
       left,
@@ -82,13 +66,12 @@ class InfoBox extends Component {
       color,
       explanationText,
       backgroundColor,
-      wordsToHightlight,
       padding
     } = this.props
     const { open } = this.state
     const paddingStyle = getPadding(padding)
     const rotate = open ? '0deg' : '180deg'
-    
+
     if (triangle) {
       return (
         <TouchableOpacity
@@ -177,9 +160,7 @@ class InfoBox extends Component {
                   paddingBottom: heightPercentageToDP('2%')
                 }}
               >
-                {wordsToHightlight && this.renderHighlightedText()}
-                {!wordsToHightlight && explanationText} {!wordsToHightlight && <CelText type={'H5'} weight={'700'} color={STYLES.COLORS.WHITE}>{boldText}</CelText>}
-
+                {explanationText} <CelText type={'H5'} weight={'700'} color={STYLES.COLORS.WHITE}>{boldText}</CelText>
               </CelText>
             </View>
           )}
