8754385ec290e2807e7ace1b2687919052f5b60f
nemanjak <31892666+nemanjakrstonic@users.noreply.github.com>
2019-10-09 16:08:16 +0200

    CN-4057, CN-4031, CN-4032, CN-4140 Bug Fixes (#321)
    * CN-4057 Added Collateral Coin Name
    
    Active Loan
    
    * CN-4032 Loan details screen
    
    - Added thousands separator
    - Added coin_loan_asset to LoanRequestDetails
    
    * CN-4140 CelSelect arrow
    
    * CN-4031 Profile screen user name fix

--- app/components/screens/LoanRequestDetails/LoanRequestDetails.js
+++ app/components/screens/LoanRequestDetails/LoanRequestDetails.js
@@ -84,25 +84,25 @@ class LoanRequestDetails extends Component {
         return <BasicSection key={sectionType} label={"Unlocked Collateral"} value={formatter.crypto(activeLoan.amount_collateral_crypto, activeLoan.collateral_coin)}/>;
       case "estimated:collateral":
         return <CardSection key={sectionType}
-                            title={activeLoan.uiProps.collateral}
-                            cardText={[LOAN_STATUS.PENDING].includes(activeLoan.status) && "Exact collateral amount would be determined upon approval"}
-                            coin={activeLoan.collateral_coin}
-                            coinAmount={activeLoan.loan_collateral_crypto}/>;
+          title={activeLoan.uiProps.collateral}
+          cardText={[LOAN_STATUS.PENDING].includes(activeLoan.status) && "Exact collateral amount would be determined upon approval"}
+          coin={activeLoan.collateral_coin}
+          coinAmount={activeLoan.loan_collateral_crypto}/>;
       case "collateral":
-        return <BasicSection key={sectionType} label={"Locked Collateral"} value={formatter.crypto(activeLoan.amount_collateral_crypto, activeLoan.collateral_coin)}/>;
+        return <BasicSection key={sectionType} label={"Locked Collateral"} value={[formatter.crypto(activeLoan.amount_collateral_crypto, activeLoan.collateral_coin), activeLoan.coin]}/>;
       case "term":
         return <BasicSection key={sectionType} label={"Term Length"} value={`${activeLoan.term_of_loan} months`}/>;
       case "annualInterest":
         return <BasicCardSection key={sectionType} label={"Annual Interest Rate"} coin={activeLoan.coin_loan_asset}
-                                 value={activeLoan.interest}
-                                 monthly={activeLoan.monthly_payment}
-                                 total={activeLoan.total_interest}/>;
+          value={activeLoan.interest}
+          monthly={activeLoan.monthly_payment}
+          total={activeLoan.total_interest}/>;
       case "marginCall":
         return activeLoan.margin_call && <CardSection key={sectionType} title={`${activeLoan.coin} Margin Call At:`} amount={activeLoan.margin_call_price}
-                            cardText={`If ${activeLoan.coin} drops below ${formatter.usd(activeLoan.margin_call_price)} you will get a notification asking for additional collateral.`} />;
+          cardText={`If ${activeLoan.coin} drops below ${formatter.usd(activeLoan.margin_call_price)} you will get a notification asking for additional collateral.`} />;
       case "liquidation":
         return activeLoan.margin_call && <CardSection key={sectionType} title={"Liquidation At:"} amount={activeLoan.liquidation_call_price}
-                             cardText={`If ${activeLoan.coin} drops below ${formatter.usd(activeLoan.liquidation_call_price)} we will sell some of your collateral to cover the margin.`}/>;
+          cardText={`If ${activeLoan.coin} drops below ${formatter.usd(activeLoan.liquidation_call_price)} we will sell some of your collateral to cover the margin.`}/>;
       // case "firstInterest":
       //   return  <BasicSection key={sectionType} label={"First Interest Payment Due"} value={moment(transaction.loan_data.first_interest).format("D MMM YYYY")}/>;
       // case "nextInterest":
@@ -121,6 +121,7 @@ class LoanRequestDetails extends Component {
 
     const style = LoanRequestDetailsStyle();
 
+    const loanCoinType = activeLoan.type === 'USD_LOAN' ? `${formatter.usd(formatter.floor10(activeLoan.loan_amount_usd, -2))}` : `${activeLoan.loan_amount} ${activeLoan.coin_loan_asset}`
 
     const sections = activeLoan.uiSections;
 
@@ -130,12 +131,12 @@ class LoanRequestDetails extends Component {
           <View style={style.status}>
             <Icon name={"TransactionLoan"} fill={activeLoan.uiProps.color} width={"25"} height={"25"}/>
             <CelText type={"H5"} color={activeLoan.uiProps.color}
-                     margin={"0 5 0 0"}>{activeLoan.uiProps.displayText}</CelText>
+              margin={"0 5 0 0"}>{activeLoan.uiProps.displayText}</CelText>
           </View>
-          <CelText type={"H2"} weight={"600"} margin={"5 0 5 0"}>{`$ ${activeLoan.loan_amount}`}</CelText>
+          <CelText type={"H2"} weight={"600"} margin={"5 0 5 0"}>{loanCoinType}</CelText>
         </View>
 
-         {sections.map(this.renderSection)}
+        {sections.map(this.renderSection)}
 
         <CelButton
           basic
