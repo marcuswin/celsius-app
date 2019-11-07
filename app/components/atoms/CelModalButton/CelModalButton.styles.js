ce7151427a4b880d29880bbb9118c9eb9f8fbb07
Srdjan Djenader <djenka76@gmail.com>
2019-11-07 10:02:19 +0100

    [DRAFT] CN4260 cel modal button atom (#396)
    * CN-4260 created modal button template
    
    * CN-4260 removed case from default in switch

---
+++ app/components/atoms/CelModalButton/CelModalButton.styles.js
@@ -0,0 +1,28 @@
+// import STYLES from '../../../constants/STYLES';
+import { getThemedStyle } from "../../../utils/styles-util";
+
+const base = {
+  container: {
+    flex: 1,
+  },
+  buttonStyle: {
+    flex: 1,
+    borderWidth: 2,
+    borderStyle: "solid",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+  },
+};
+
+const themed = {
+  light: {},
+
+  dark: {},
+
+  celsius: {},
+};
+
+const CelModalButtonStyle = () => getThemedStyle(base, themed);
+
+export default CelModalButtonStyle;
