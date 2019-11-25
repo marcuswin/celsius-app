2ce44fbb4ccff624581050a1e24dfe39d6e1879b
Srdjan Djenader <djenka76@gmail.com>
2019-11-25 08:52:06 +0100

    CN 4395 created lose tier modal

---
+++ app/components/modals/LoseTierModal/LoseTierModal.styles.js
@@ -0,0 +1,20 @@
+// import STYLES from '../../../constants/STYLES';
+import { getThemedStyle } from "../../../utils/styles-util";
+
+const base = {
+  container: {
+    flex: 1,
+  },
+};
+
+const themed = {
+  light: {},
+
+  dark: {},
+
+  celsius: {},
+};
+
+const LoseTierModalStyle = () => getThemedStyle(base, themed);
+
+export default LoseTierModalStyle;
