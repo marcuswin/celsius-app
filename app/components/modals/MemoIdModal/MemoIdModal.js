f2c0b59d7b59bc45a63685543f2726e2d6735f29
Srdjan Djenader <djenka76@gmail.com>
2019-11-25 19:21:28 +0100

    CN-4399 memo id modal converted infomodal to celmodal

--- app/components/modals/MemoIdModal/MemoIdModal.js
+++ app/components/modals/MemoIdModal/MemoIdModal.js
@@ -1,27 +1,40 @@
 import React from "react";
+import { ScrollView } from "react-native";
 
-import InfoModal from "../InfoModalNew/InfoModal.js";
-import MemoIdModalStyle from "./MemoIdModal.styles";
 import { MODALS } from "../../../constants/UI";
+import CelText from "../../atoms/CelText/CelText";
+import CelButton from "../../atoms/CelButton/CelButton";
+import CelModal from "../CelModal/CelModal";
+import { heightPercentageToDP } from "../../../utils/styles-util";
 
 const MemoIdModal = props => {
-  const style = MemoIdModalStyle(props.theme);
   const { closeModal, coin } = props;
   return (
-    <InfoModal
-      style={style.container}
-      name={MODALS.MEMO_ID_MODAL}
-      heading={coin === "EOS" ? `EOS (EOS) Memo ID` : `Stellar (XLM) Memo ID`}
-      paragraphs={[
-        " Memo ID is used to determine what account a given transaction should be  assigned and credited to.",
-        `Quoting the Memo ID with the ${
-          coin === "EOS" ? "EOS" : "Stellar"
-        } wallet address ensures that your transaction is uniquely identified and processed successfully.`,
-        "Exchanges require Memo ID, so please make sure to provide it, or you risk losing your money.",
-      ]}
-      yesCopy={"Got it"}
-      onYes={closeModal}
-    />
+    <CelModal name={MODALS.MEMO_ID_MODAL}>
+      <ScrollView style={{ maxHeight: heightPercentageToDP("60%") }}>
+        <CelText align="center" type="H2" margin="0 25 32 25" weight="bold">
+          {coin === "EOS" ? `EOS (EOS) Memo ID` : `Stellar (XLM) Memo ID`}
+        </CelText>
+        <CelText align="center" type="H4" margin="0 25 24 25">
+          Memo ID is used to determine what account a given transaction should
+          be assigned and credited to.
+        </CelText>
+        <CelText align="center" type="H4" margin="0 25 24 25">
+          {`Quoting the Memo ID with the ${
+            coin === "EOS" ? "EOS" : "Stellar"
+          } wallet address ensures that your
+        transaction is uniquely identified and processed successfully.`}
+        </CelText>
+        <CelText align="center" type="H4" margin="0 25 24 25">
+          Exchanges require Memo ID, so please make sure to provide it, or you
+          risk losing your money.
+        </CelText>
+
+        <CelButton margin={"0 0 20 0"} onPress={closeModal}>
+          Got it
+        </CelButton>
+      </ScrollView>
+    </CelModal>
   );
 };
 
